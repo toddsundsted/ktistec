2de4a4b3894026afcda748ffd99f53107887a540
Todd Sundsted <todd@sundsted.com>
2020-04-02 21:50:40 -0400

    Model CRUD operations.

--- spec/framework/model_spec.cr
+++ spec/framework/model_spec.cr
@@ -38,4 +38,195 @@ Spectator.describe Balloon::Model do
     Balloon.database.exec "DROP TABLE foo_bar_models"
     Balloon.database.exec "DROP TABLE not_nil_models"
   end
+
+  describe ".new" do
+    it "creates a new instance" do
+      expect(FooBarModel.new(foo: "Foo").foo).to eq("Foo")
+      expect(FooBarModel.new.foo).to be_nil
+    end
+
+    it "creates a new instance" do
+      expect(NotNilModel.new(val: "Val").val).to eq("Val")
+    end
+  end
+
+  describe "#assign" do
+    it "bulk assigns properties" do
+      expect(FooBarModel.new.assign(foo: "Foo").foo).to eq("Foo")
+      expect(FooBarModel.new.assign.foo).to be_nil
+    end
+
+    it "bulk assigns properties" do
+      expect(NotNilModel.new(val: "Val").assign(val: "").val).to eq("")
+    end
+  end
+
+  describe "#==" do
+    it "returns true if all properties are equal" do
+      a = FooBarModel.new(foo: "Foo")
+      b = FooBarModel.new(foo: "Foo")
+      expect(a).to eq(b)
+    end
+
+    it "returns true if all properties are equal" do
+      a = NotNilModel.new(val: "Val")
+      b = NotNilModel.new(val: "Val")
+      expect(a).to eq(b)
+    end
+  end
+
+  describe ".count" do
+    it "returns the count of persisted instances" do
+      expect(FooBarModel.count).to eq(0)
+    end
+
+    it "returns the count of persisted instances" do
+      expect(NotNilModel.count).to eq(0)
+    end
+  end
+
+  describe ".all" do
+    it "returns all persisted instances" do
+      saved_model = FooBarModel.new.save
+      expect(FooBarModel.all).to eq([saved_model])
+    end
+
+    it "returns all persisted instances" do
+      saved_model = NotNilModel.new(val: "Val").save
+      expect(NotNilModel.all).to eq([saved_model])
+    end
+  end
+
+  describe ".find" do
+    context "given the id" do
+      it "finds the saved instance" do
+        saved_model = FooBarModel.new.save
+        expect(FooBarModel.find(saved_model.id)).not_to be(saved_model)
+        expect(FooBarModel.find(saved_model.id)).to eq(saved_model)
+      end
+
+      it "finds the updated instance" do
+        updated_model = FooBarModel.new.save.save
+        expect(FooBarModel.find(updated_model.id)).not_to be(updated_model)
+        expect(FooBarModel.find(updated_model.id)).to eq(updated_model)
+      end
+
+      it "finds the saved instance" do
+        saved_model = NotNilModel.new(val: "Val").save
+        expect(NotNilModel.find(saved_model.id)).not_to be(saved_model)
+        expect(NotNilModel.find(saved_model.id)).to eq(saved_model)
+      end
+    end
+
+    context "given properties" do
+      it "finds the saved instance" do
+        saved_model = FooBarModel.new(foo: "Foo", bar: "Bar").save
+        expect(FooBarModel.find(foo: "Foo")).not_to be(saved_model)
+        expect(FooBarModel.find(foo: "Foo")).to eq(saved_model)
+      end
+
+      it "finds the updated instance" do
+        updated_model = FooBarModel.new(foo: "Foo", bar: "Bar").save.assign(foo: "Bar").save
+        expect(FooBarModel.find(foo: "Bar")).not_to be(updated_model)
+        expect(FooBarModel.find(foo: "Bar")).to eq(updated_model)
+      end
+
+      it "finds the saved instance" do
+        saved_model = NotNilModel.new(val: "Val").save
+        expect(NotNilModel.find(val: "Val")).not_to be(saved_model)
+        expect(NotNilModel.find(val: "Val")).to eq(saved_model)
+      end
+    end
+  end
+
+  describe ".where" do
+    it "returns the saved instances" do
+      saved_model = FooBarModel.new(foo: "Foo", bar: "Bar").save
+      expect(FooBarModel.where(foo: "Foo")).to eq([saved_model])
+      expect(FooBarModel.where(foo: "Bar")).to be_empty
+    end
+
+    it "returns the saved instances" do
+      saved_model = FooBarModel.new(foo: "Foo", bar: "Bar").save
+      expect(FooBarModel.where("foo = ?", "Foo")).to eq([saved_model])
+      expect(FooBarModel.where("foo = ?", "Bar")).to be_empty
+    end
+
+    it "returns the updated instances" do
+      updated_model = FooBarModel.new(foo: "Foo", bar: "Bar").save.assign(foo: "Bar").save
+      expect(FooBarModel.where(foo: "Bar")).to eq([updated_model])
+      expect(FooBarModel.where(foo: "Foo")).to be_empty
+    end
+
+    it "returns the updated instances" do
+      updated_model = FooBarModel.new(foo: "Foo", bar: "Bar").save.assign(foo: "Bar").save
+      expect(FooBarModel.where("foo = ?", "Bar")).to eq([updated_model])
+      expect(FooBarModel.where("foo = ?", "Foo")).to be_empty
+    end
+
+    it "returns the saved instances" do
+      saved_model = NotNilModel.new(val: "Val").save
+      expect(NotNilModel.where(val: "Val")).to eq([saved_model])
+      expect(NotNilModel.where(val: "")).to be_empty
+    end
+
+    it "returns the saved instances" do
+      saved_model = NotNilModel.new(val: "Val").save
+      expect(NotNilModel.where("val = ?", "Val")).to eq([saved_model])
+      expect(NotNilModel.where("val = ?", "")).to be_empty
+    end
+  end
+
+  describe "#save" do
+    context "new instance" do
+      it "saves a new instance" do
+        expect{FooBarModel.new.save}.to change{FooBarModel.count}.by(1)
+      end
+
+      it "assigns an id" do
+        new_model = FooBarModel.new
+        expect{new_model.save}.to change{new_model.id}
+      end
+
+      it "saves the properties" do
+        saved_model = FooBarModel.new(foo: "Foo", bar: "Bar").save
+        expect(FooBarModel.find(saved_model.id).foo).to eq("Foo")
+        expect(FooBarModel.find(saved_model.id).bar).to eq("Bar")
+      end
+
+      it "saves the properties" do
+        saved_model = NotNilModel.new(val: "Val").save
+        expect(NotNilModel.find(saved_model.id).val).to eq("Val")
+      end
+    end
+
+    context "existing instance" do
+      it "does not save a new instance" do
+        expect{FooBarModel.new.save.save}.to change{FooBarModel.count}.by(1)
+      end
+
+      it "does not assign an id" do
+        saved_model = FooBarModel.new.save
+        expect{saved_model.save}.not_to change{saved_model.id}
+      end
+
+      it "updates the properties" do
+        updated_model = FooBarModel.new(foo: "Foo", bar: "Bar").save.assign(foo: "Bar").save
+        expect(FooBarModel.find(updated_model.id).foo).to eq("Bar")
+        expect(FooBarModel.find(updated_model.id).bar).to eq("Bar")
+      end
+
+      it "updates the properties" do
+        updated_model = NotNilModel.new(val: "Val").save.assign(val: "").save
+        expect(NotNilModel.find(updated_model.id).val).to eq("")
+      end
+    end
+  end
+
+  describe "#destroy" do
+    it "destroys the persisted instance" do
+      saved_model = FooBarModel.new.save
+      expect{saved_model.destroy}.to change{FooBarModel.count}.by(-1)
+    end
+  end
 end
--- src/framework/model.cr
+++ src/framework/model.cr
@@ -20,8 +20,225 @@ module Balloon
       end
     end
 
+    module ClassMethods
+      # Returns the table name.
+      #
+      def table_name
+        @@table_name ||= Utils.table_name({{@type}})
+      end
+
+      # Returns the count of saved instances.
+      #
+      def count
+        Balloon.database.scalar("SELECT COUNT(*) FROM #{table_name}").as(Int)
+      end
+
+      # Returns all instances.
+      #
+      def all
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          columns = {{vs.map(&.stringify)}}.join(",")
+          Balloon.database.query_all(
+            "SELECT #{columns} FROM #{table_name}"
+          ) do |rs|
+            {{@type}}.new(
+              {% for v in vs %}
+                {{v}}: rs.read({{v.type}}),
+              {% end %}
+            )
+          end
+        {% end %}
+      end
+
+      # Finds the saved instance.
+      #
+      def find(id)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          columns = {{vs.map(&.stringify)}}.join(",")
+          conditions = "id = ?"
+          Balloon.database.query_one(
+            "SELECT #{columns} FROM #{table_name} WHERE #{conditions}",
+            id.not_nil!
+          ) do |rs|
+            {{@type}}.new(
+              {% for v in vs %}
+                {{v}}: rs.read({{v.type}}),
+              {% end %}
+            )
+          end
+        {% end %}
+      end
+
+      # Finds the saved instance.
+      #
+      def find(**options)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          columns = {{vs.map(&.stringify)}}.join(",")
+          conditions = {{vs.map(&.stringify)}}.select do |v|
+            options.has_key?(v)
+          end.map do |v|
+            "#{v} = ?"
+          end.join(",")
+          Balloon.database.query_one(
+            "SELECT #{columns} FROM #{table_name} WHERE #{conditions}",
+            *options.values
+          ) do |rs|
+            {{@type}}.new(
+              {% for v in vs %}
+                {{v}}: rs.read({{v.type}}),
+              {% end %}
+            )
+          end
+        {% end %}
+      end
+
+      # Returns saved instances.
+      #
+      def where(**options)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          columns = {{vs.map(&.stringify)}}.join(",")
+          conditions = {{vs.map(&.stringify)}}.select do |v|
+            options.has_key?(v)
+          end.map do |v|
+            "#{v} = ?"
+          end.join(",")
+          Balloon.database.query_all(
+            "SELECT #{columns} FROM #{table_name} WHERE #{conditions}",
+            *options.values
+          ) do |rs|
+            {{@type}}.new(
+              {% for v in vs %}
+                {{v}}: rs.read({{v.type}}),
+              {% end %}
+            )
+          end
+        {% end %}
+      end
+
+      # Returns saved instances.
+      #
+      def where(conditions : String, *arguments)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          columns = {{vs.map(&.stringify)}}.join(",")
+          Balloon.database.query_all(
+            "SELECT #{columns} FROM #{table_name} WHERE #{conditions}",
+            *arguments
+          ) do |rs|
+            {{@type}}.new(
+              {% for v in vs %}
+                {{v}}: rs.read({{v.type}}),
+              {% end %}
+            )
+          end
+        {% end %}
+      end
+    end
+
+    module InstanceMethods
+      # Initializes the new instance.
+      #
+      def initialize(**options)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          {% for v in vs %}
+            if (o = options[{{v.symbolize}}]?)
+              self.{{v}} = o
+            end
+          {% end %}
+        {% end %}
+        super
+      end
+
+      # Returns true if all properties are equal.
+      #
+      def ==(other : self)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          if
+            {% for v in vs %}
+              self.{{v}} == other.{{v}} &&
+            {% end %}
+            self.id == other.id
+            true
+          else
+            false
+          end
+        {% end %}
+      end
+
+      # Bulk assigns properties.
+      #
+      def assign(**options)
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          {% for v in vs %}
+            if (o = options[{{v.symbolize}}]?)
+              self.{{v}} = o
+            end
+          {% end %}
+        {% end %}
+        self
+      end
+
+      # Returns the table name.
+      #
+      def table_name
+        @@table_name ||= Utils.table_name({{@type}})
+      end
+
+      # Saves the instance.
+      #
+      def save
+        {% begin %}
+          {% vs = @type.instance_vars.select(&.annotation(Persistent)) %}
+          if @id
+            conditions = {{vs.map(&.stringify)}}.map do |v|
+              "#{v} = ?"
+            end.join(",")
+            Balloon.database.exec(
+              "UPDATE #{table_name} SET #{conditions} WHERE id = ?",
+              {% for v in vs %}
+                {{v}},
+              {% end %}
+              @id
+            )
+          else
+            columns = {{vs.map(&.stringify)}}.join(",")
+            conditions = (["?"] * {{vs.size}}).join(",")
+            @id = Balloon.database.exec(
+              "INSERT INTO #{table_name} (#{columns}) VALUES (#{conditions})",
+              {% for v in vs %}
+                {{v}},
+              {% end %}
+            ).last_insert_id
+          end
+        {% end %}
+        self
+      end
+
+      # Destroys the instance.
+      #
+      def destroy
+        Balloon.database.exec("DELETE FROM #{table_name} WHERE id = ?", @id)
+        @id = nil
+        self
+      end
+    end
+
+    macro included
+      extend ClassMethods
+      include InstanceMethods
+    end
+
     @[Persistent]
     property id : Int64?
+
+    @@table_name : String?
   end
 end
 

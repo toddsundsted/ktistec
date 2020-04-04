dceeacc3c3c7795b9465b871d51a53fb8a497001
Todd Sundsted <todd@sundsted.com>
2020-04-04 11:12:30 -0400

    Shortcuts for HTTP errors.

--- src/framework/controller.cr
+++ src/framework/controller.cr
@@ -5,6 +5,24 @@ module Balloon
     macro host
       Balloon.config.host
     end
+
+    macro bad_request
+      body = {msg: "Bad Request"}
+      env.response.content_type = "application/json"
+      halt env, status_code: 400, response: body.to_json
+    end
+
+    macro not_found
+      body = {msg: "Not Found"}
+      env.response.content_type = "application/json"
+      halt env, status_code: 404, response: body.to_json
+    end
+
+    macro server_error
+      body = {msg: "Server Error"}
+      env.response.content_type = "application/json"
+      halt env, status_code: 500, response: body.to_json
+    end
   end
 end
 

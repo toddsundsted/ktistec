b676c563f4924b1791539e5449883314b8c91a65
Todd Sundsted <todd@sundsted.com>
2020-04-08 05:55:33 -0400

    Implement authentication.

---
+++ spec/framework/auth_spec.cr
@@ -0,0 +1,66 @@
+require "../spec_helper"
+
+class FooBarController
+  include Balloon::Controller
+
+  get "/foo/bar/auth" do |env|
+    {actor: env.current_actor?, session: env.session?}.to_json
+  end
+end
+
+Spectator.describe Balloon::Auth do
+  before_each { Balloon.database.exec "BEGIN TRANSACTION" }
+  after_each { Balloon.database.exec "ROLLBACK" }
+
+  def random_string
+    ('a'..'z').to_a.shuffle.first(8).join
+  end
+
+  let(username) { random_string }
+  let(password) { random_string }
+
+  let(actor) { Actor.new(username, password).save }
+  let(session) { Session.new(actor).save }
+  let(payload) { {sub: actor.id, jti: session.session_key, iat: Time.utc} }
+  let(jwt) { Balloon::JWT.encode(payload) }
+
+  describe "get /foo/bar/auth" do
+    it "successfully authenticates" do
+      get "/foo/bar/auth", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+      expect(response.status_code).to eq(200)
+      expect(JSON.parse(response.body).dig("actor", "id")).to eq(actor.id)
+      expect(JSON.parse(response.body).dig("session", "session_key")).to eq(session.session_key)
+    end
+
+    it "successfully authenticates" do
+      get "/foo/bar/auth", HTTP::Headers{"Cookie" => "one=two; AuthToken=#{jwt}"}
+      expect(response.status_code).to eq(200)
+      expect(JSON.parse(response.body).dig("actor", "id")).to eq(actor.id)
+      expect(JSON.parse(response.body).dig("session", "session_key")).to eq(session.session_key)
+    end
+
+    it "fails to authenticate" do
+      get "/foo/bar/auth"
+      expect(response.status_code).to eq(401)
+      expect(JSON.parse(response.body)).to eq({"msg" => "Forbidden"})
+    end
+
+    context "invalid session" do
+      let(payload) { {sub: actor.id, jti: "invalid session key", iat: Time.utc} }
+
+      it "fails to authenticate" do
+        get "/foo/bar/auth", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+        expect(response.status_code).to eq(401)
+      end
+    end
+
+    context "invalid time" do
+      let(payload) { {sub: actor.id, jti: session.session_key, iat: Time.utc - 1.year} }
+
+      it "fails to authenticate" do
+        get "/foo/bar/auth", HTTP::Headers{"Cookie" => "one=two; AuthToken=#{jwt}"}
+        expect(response.status_code).to eq(401)
+      end
+    end
+  end
+end
---
+++ src/framework/auth.cr
@@ -0,0 +1,55 @@
+require "./framework"
+
+class HTTP::Server::Context
+  property? current_actor : Actor?
+  property? session : Session?
+end
+
+module Balloon
+  # Authentication middleware.
+  #
+  class Auth < Kemal::Handler
+    def call(env)
+      return call_next env if exclude_match?(env)
+
+      if (value = check_authorization(env)) || (value = check_cookie(env))
+        if payload = Balloon::JWT.decode(value)
+          begin
+            if session = Session.find(session_key: payload["jti"].as_s)
+              if Time.parse_iso8601(payload["iat"].as_s) > Time.utc - 1.month
+                env.current_actor = session.actor
+                env.session = session
+                return call_next(env)
+              end
+            end
+          rescue ex : DB::Error
+            raise ex unless ex.message == "no rows"
+          end
+        end
+      end
+
+      headers = HTTP::Headers.new
+      env.response.status_code = 401
+      env.response.headers["Content-Type"] = "application/json"
+      env.response.print "{\"msg\":\"Forbidden\"}"
+    end
+
+    private def check_authorization(env)
+      if value = env.request.headers["Authorization"]?
+        if value.size > 0 && value.starts_with?("Bearer ")
+          value.split(" ").last
+        end
+      end
+    end
+
+    private def check_cookie(env)
+      if value = env.request.headers["Cookie"]?
+        if value.size > 0 && value.includes?("AuthToken=")
+          value.split("; ").find(&.starts_with?("AuthToken")).try(&.split("=").last)
+        end
+      end
+    end
+  end
+
+  add_handler Auth.new
+end

59731b4b613afa90666334b8384935a5d61c93e1
Todd Sundsted <todd@sundsted.com>
2020-04-10 06:40:28 -0400

    Add sessions controller.

---
+++ spec/controllers/sessions_spec.cr
@@ -0,0 +1,108 @@
+require "../spec_helper"
+
+Spectator.describe SessionsController do
+  before_each { Balloon.database.exec "BEGIN TRANSACTION" }
+  after_each { Balloon.database.exec "ROLLBACK" }
+
+  def random_string
+    ('a'..'z').to_a.shuffle.first(8).join
+  end
+
+  let(username) { random_string }
+  let(password) { random_string }
+
+  let!(actor) { Actor.new(username, password).save }
+  let(session) { Session.new(actor).save }
+  let(payload) { {sub: actor.id, jti: session.session_key, iat: Time.utc} }
+  let(jwt) { Balloon::JWT.encode(payload) }
+
+  describe "GET /sessions" do
+    it "responds with HTML" do
+      get "/sessions", HTTP::Headers{"Accept" => "text/html"}
+      expect(response.status_code).to eq(200)
+      expect(XML.parse_html(response.body).xpath_nodes("//form[./input]")).not_to be_empty
+    end
+
+    it "responds with JSON" do
+      get "/sessions", HTTP::Headers{"Accept" => "application/json"}
+      expect(response.status_code).to eq(200)
+      expect(JSON.parse(response.body).as_h.keys).to have("username", "password")
+    end
+  end
+
+  describe "POST /sessions" do
+    it "redirects if params are missing" do
+      headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+      post "/sessions", headers
+      expect(response.status_code).to eq(302)
+      expect(response.headers.to_a).to have({"Location", ["/sessions"]})
+    end
+
+    it "redirects if params are missing" do
+      headers = HTTP::Headers{"Content-Type" => "application/json"}
+      post "/sessions", headers
+      expect(response.status_code).to eq(302)
+      expect(response.headers.to_a).to have({"Location", ["/sessions"]})
+    end
+
+    it "rerenders if params are incorrect" do
+      headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+      body = "username=foo&password=bar"
+      post "/sessions", headers, body
+      expect(response.status_code).to eq(403)
+      expect(XML.parse_html(response.body).xpath_nodes("//form[./input]")).not_to be_empty
+    end
+
+    it "rerenders if params are incorrect" do
+      headers = HTTP::Headers{"Content-Type" => "application/json"}
+      body = {username: "foo", password: "bar"}.to_json
+      post "/sessions", headers, body
+      expect(response.status_code).to eq(403)
+      expect(JSON.parse(response.body).as_h.keys).to have("msg", "username", "password")
+    end
+
+    it "sets cookie and redirects " do
+      headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+      body = "username=#{username}&password=#{password}"
+      post "/sessions", headers, body
+      expect(response.status_code).to eq(302)
+      expect(response.headers["Set-Cookie"]).to be_truthy
+    end
+
+    it "returns token" do
+      headers = HTTP::Headers{"Content-Type" => "application/json"}
+      body = {username: username, password: password}.to_json
+      post "/sessions", headers, body
+      expect(response.status_code).to eq(200)
+      expect(JSON.parse(response.body)["jwt"]).to be_truthy
+    end
+  end
+
+  describe "POST /sessions/forget" do
+    it "fails to authenticate" do
+      post "/sessions/forget", HTTP::Headers{"Accept" => "text/html"}
+      expect(response.status_code).to eq(401)
+      expect(XML.parse_html(response.body).xpath_nodes("/html//title").first.text).to eq("Unauthorized")
+    end
+
+    it "fails to authenticate" do
+      post "/sessions/forget", HTTP::Headers{"Accept" => "application/json"}
+      expect(response.status_code).to eq(401)
+      expect(JSON.parse(response.body)["msg"]).to eq("Unauthorized")
+    end
+
+    it "destroys session and redirects" do
+      headers = HTTP::Headers{"Cookie" => "AuthToken=#{jwt}", "Accept" => "text/html"}
+      expect{post "/sessions/forget", headers}.to change{Session.count}.by(-1)
+      expect(response.status_code).to eq(302)
+      expect(response.headers.to_a).to have({"Location", ["/sessions"]})
+    end
+
+    it "destroys session and redirects" do
+      headers = HTTP::Headers{"Authorization" => "Bearer #{jwt}", "Accept" => "application/json"}
+      expect{post "/sessions/forget", headers}.to change{Session.count}.by(-1)
+      expect(response.status_code).to eq(302)
+      expect(response.headers.to_a).to have({"Location", ["/sessions"]})
+    end
+  end
+end
---
+++ src/controllers/sessions.cr
@@ -0,0 +1,72 @@
+require "../framework"
+
+class SessionsController
+  include Balloon::Controller
+
+  skip_auth ["/sessions"], "GET"
+  skip_auth ["/sessions"], "POST"
+
+  get "/sessions" do |env|
+    message = username = password = nil
+
+    if accepts?("text/html")
+      env.response.content_type = "text/html"
+      render "src/views/pages/login.ecr"
+    else
+      env.response.content_type = "application/json"
+      {username: username, password: password}.to_json
+    end
+  end
+
+  post "/sessions" do |env|
+    username, password = params(env)
+
+    if actor = actor?(username, password)
+      session = Session.new(actor).save
+      payload = {sub: actor.id, jti: session.session_key, iat: Time.utc}
+      jwt = Balloon::JWT.encode(payload)
+
+      if accepts?("text/html")
+        env.response.cookies["AuthToken"] = jwt
+        env.redirect "/actors/#{actor.username}"
+      else
+        env.response.content_type = "application/json"
+        {jwt: jwt}.to_json
+      end
+    else
+      message = "invalid username or password"
+
+      env.response.status_code = 403
+      if accepts?("text/html")
+        env.response.content_type = "text/html"
+        render "src/views/pages/login.ecr"
+      else
+        env.response.content_type = "application/json"
+        {msg: message, username: username, password: password}.to_json
+      end
+    end
+  rescue KeyError
+    env.redirect "/sessions"
+  end
+
+  post "/sessions/forget" do |env|
+    if session = env.session?
+      session.destroy
+    end
+    env.redirect "/sessions"
+  end
+
+  private def self.params(env)
+    params = accepts?("text/html") ? env.params.body : env.params.json
+    ["username", "password"].map { |p| params[p].as(String) }
+  end
+
+  private def self.actor?(username, password)
+    actor = Actor.find(username: username)
+    if actor.valid_password?(password)
+      actor
+    end
+  rescue
+    nil
+  end
+end

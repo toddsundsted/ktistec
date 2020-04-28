b297b5aa3c22959b4cc3c3c395defd2b78720339
Todd Sundsted <todd@sundsted.com>
2020-04-28 08:18:00 -0400

    Basic expansion of JSON-LD documents.

--- spec/framework/json_ld_spec.cr
+++ spec/framework/json_ld_spec.cr
@@ -6,4 +6,261 @@ Spectator.describe Balloon::JSON_LD do
       expect(Balloon::JSON_LD::CONTEXTS).not_to be_empty
     end
   end
+
+  describe ".expand" do
+    let(json) { JSON.parse(%({"name":"Foo Bar"})) }
+
+    it "returns a JSON document" do
+      expect(described_class.expand(json)).to be_a(JSON::Any)
+    end
+  end
+
+  context "given plain JSON document" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "name": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "finds no terms" do
+        expect(json.as_h.keys).to be_empty
+      end
+    end
+  end
+
+  context "given JSON document with vocabulary" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@type": "https://lock",
+            "https://name": "Foo Bar Baz",
+            "https://page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns terms" do
+        expect(json.as_h.keys).to match_array(["@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  mock Balloon::JSON_LD::Loader do
+    stub load(url) do
+      case url
+      when "https://vocab"
+        JSON.parse(<<-JSON
+            {
+              "@context": {
+                "Lock": "https://lock",
+                "name": "https://name",
+                "page": {
+                  "@id": "https://page",
+                  "@type": "@id"
+                }
+              }
+            }
+          JSON
+        )
+      else
+        raise "not found"
+      end
+    end
+  end
+
+  context "given JSON-LD document with embedded context" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": {
+              "Lock": "https://lock",
+              "name": "https://name",
+              "page": {
+                "@id": "https://page",
+                "@type": "@id"
+              }
+            },
+            "@type": "Lock",
+            "name": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document with remote context" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": "https://vocab",
+            "@type": "Lock",
+            "name": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document with mixed context" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": [
+              "https://vocab",
+              {
+                "full": "https://full"
+              }
+            ],
+            "@type": "Lock",
+            "full": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://full", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document using compact IRIs" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": {
+              "xx": "https://"
+            },
+            "@type": "xx:lock",
+            "xx:name": "Foo Bar Baz",
+            "xx:page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document using compact IRIs" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": {
+              "xx": "https://",
+              "Lock": "xx:lock",
+              "name": "xx:name",
+              "page": {
+                "@id": "xx:page",
+                "@type": "@id"
+              }
+            },
+            "@type": "Lock",
+            "name": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document using compact IRIs" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": [
+              {
+                "xx": "https://"
+              },
+              {
+                "Lock": "xx:lock",
+                "name": "xx:name",
+                "page": {
+                  "@id": "xx:page",
+                  "@type": "@id"
+                }
+              }
+            ],
+            "@type": "Lock",
+            "name": "Foo Bar Baz",
+            "page": "https://test/"
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://name", "https://page"]).in_any_order
+        expect(json["@type"]).to eq("https://lock")
+      end
+    end
+  end
+
+  context "given JSON-LD document with nested objects" do
+    let(json) do
+      described_class.expand(JSON.parse(<<-JSON
+          {
+            "@context": [
+              "https://vocab",
+              {
+                "base": "https://base"
+              }
+            ],
+            "@type": "Lock",
+            "base": [{
+              "name": "Foo Bar Baz",
+              "page": "https://test/"
+            }]
+          }
+        JSON
+      ))
+    end
+
+    describe "#[]" do
+      it "returns mapped terms" do
+        expect(json.as_h.keys).to match_array(["@context", "@type", "https://base"]).in_any_order
+        expect(json["https://base"][0].as_h.keys).to match_array(["https://name", "https://page"]).in_any_order
+      end
+    end
+  end
 end
--- src/framework/json_ld.cr
+++ src/framework/json_ld.cr
@@ -1,7 +1,118 @@
 require "json"
+require "uri"
 
 module Balloon
   module JSON_LD
+    # Expands the JSON-LD document.
+    #
+    def self.expand(body, loader = Loader.new)
+      expand(
+        body,
+        context(body["@context"]? || empty, loader),
+        loader
+      )
+    end
+
+    private def self.empty
+      wrap(Hash(String, JSON::Any).new)
+    end
+
+    private def self.wrap(value)
+      JSON::Any.new(value)
+    end
+
+    private def self.expand(body, context, loader)
+      result = Hash(String, JSON::Any).new
+
+      body.as_h.each do |term, value|
+        value =
+          if value.as_a?
+            val = value.as_a.map do |v|
+              v.as_h? ? expand(v, context, loader) : v
+            end
+            wrap(val)
+          elsif value.as_h?
+            expand(value, context, loader)
+          else
+            value
+          end
+
+        if term.includes?(":")
+          prefix, suffix = term.split(":")
+          if context[prefix]? && !suffix.starts_with?("//")
+            result["#{context[prefix]}#{suffix}"] = value
+          else
+            result[term] = value
+          end
+        elsif term.starts_with?("@")
+          if value.as_s?
+            result[term] = _expand(value.as_s, context)
+          else
+            result[term] = value
+          end
+        elsif defn = context[term]?.try(&.as_s)
+          if defn.starts_with?("@") && value.as_s?
+            result[defn] = _expand(value.as_s, context)
+          else
+            result[defn] = value
+          end
+        end
+      end
+
+      wrap(result)
+    end
+
+    private def self.context(context, loader)
+      result = Hash(String, JSON::Any).new
+
+      if url = context.as_s?
+        context = loader.load(url)["@context"]
+      elsif array = context.as_a?
+        context = array.reduce(empty) do |a, c|
+          if u = c.as_s?
+            c = loader.load(u)["@context"]
+          end
+          wrap(a.as_h.merge(c.as_h))
+        end
+      end
+
+      context.as_h.each do |term, defn|
+        if term.includes?(":")
+          prefix, suffix = term.split(":")
+          term = "#{context[prefix]}#{suffix}"
+        end
+        if iri = defn.as_s?
+          result[term] = _expand(iri, context)
+        elsif map = defn.as_h?
+          result[term] = _expand(map["@id"].as_s, context)
+        end
+      end
+
+      wrap(result)
+    end
+
+    private def self._expand(string, context)
+      if string.includes?(":")
+        prefix, suffix = string.split(":")
+        if context[prefix]? && !suffix.starts_with?("//")
+          wrap("#{context[prefix]}#{suffix}")
+        else
+          context[string]? || wrap(string)
+        end
+      else
+        context[string]? || wrap(string)
+      end
+    end
+
+    # Loads context from cache.
+    #
+    class Loader
+      def load(url)
+        uri = URI.parse(url)
+        CONTEXTS["#{uri.host}#{uri.path}/context.jsonld"]
+      end
+    end
+
     {% begin %}
       # :nodoc:
       CONTEXTS = {

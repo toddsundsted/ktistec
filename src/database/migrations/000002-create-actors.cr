39f08a39b31ce0de922880f477a226d8462d69f1
Todd Sundsted <todd@sundsted.com>
2020-05-19 04:34:35 -0400

    Ensure `aid` is not null in the database.

--- src/database/migrations/000002-create-actors.cr
+++ src/database/migrations/000002-create-actors.cr
@@ -9,7 +9,7 @@ up do |db|
       created_at datetime NOT NULL,
       updated_at datetime NOT NULL,
       type varchar(32) NOT NULL,
-      aid varchar(255),
+      aid varchar(255) NOT NULL,
       username varchar(255),
       pem_public_key text,
       pem_private_key text,

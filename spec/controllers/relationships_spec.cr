65784bfdfeaa4d59fe43cdee27250bd8e0ca3bdb
Todd Sundsted <todd@sundsted.com>
2020-05-31 03:33:31 -0400

    Implement following/followers actions.

---
+++ spec/controllers/relationships_spec.cr
@@ -0,0 +1,251 @@
+require "../spec_helper"
+
+Spectator.describe RelationshipsController do
+  before_each { Balloon.database.exec "BEGIN TRANSACTION" }
+  after_each { Balloon.database.exec "ROLLBACK" }
+
+  describe "GET /actors/:username/:relationship" do
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Accept" => "text/html"}
+      get "/actors/0/following", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Accept" => "application/json"}
+      get "/actors/0/following", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    context "when authorized" do
+      let(actor) do
+        ActivityPub::Actor.find(Global.account.not_nil!.iri)
+      end
+      let(other) do
+        ActivityPub::Actor.new(iri: "https://unknown/#{random_string}").save
+      end
+      let(relationship) do
+        Relationship::Social::Follow.new(
+          from_iri: actor.iri,
+          to_iri: other.iri
+        ).save
+      end
+
+      sign_in
+
+      it "renders the related actors" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        get "/actors/#{actor.username}/following", headers
+        expect(response.status_code).to eq(200)
+        expect(XML.parse_html(response.body).xpath_nodes("//li[@class='actor']//a/@href").first.text).to eq(other.iri)
+      end
+
+      it "renders the related actors" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        get "/actors/#{actor.username}/following", headers
+        expect(response.status_code).to eq(200)
+        expect(JSON.parse(response.body).dig("items", 0)).to eq(other.iri)
+      end
+
+      it "returns 404 if not the current account" do
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        get "/actors/#{other.username}/following", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 404 if not the current account" do
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        get "/actors/#{other.username}/following", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        get "/actors/#{actor.username}/foobar", headers
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        get "/actors/#{actor.username}/foobar", headers
+        expect(response.status_code).to eq(400)
+      end
+    end
+  end
+
+  describe "POST /actors/:username/:relationship" do
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+      post "/actors/0/following", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Content-Type" => "application/json"}
+      post "/actors/0/following", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    context "when authorized" do
+      let(actor) do
+        ActivityPub::Actor.find(Global.account.not_nil!.iri)
+      end
+      let(other) do
+        ActivityPub::Actor.new(iri: "https://unknown/#{random_string}").save
+      end
+
+      sign_in
+
+      it "creates a relationship and redirects" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        expect{post "/actors/#{actor.username}/following", headers, "iri=#{other.iri}"}.to change{Relationship.count}.by(1)
+        expect(response.status_code).to eq(302)
+        expect(response.headers["Location"]).to eq("/actors/#{actor.username}/following")
+      end
+
+      it "creates a relationship and redirects" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        expect{post "/actors/#{actor.username}/following", headers, {iri: other.iri}.to_json}.to change{Relationship.count}.by(1)
+        expect(response.status_code).to eq(302)
+        expect(response.headers["Location"]).to eq("/actors/#{actor.username}/following")
+      end
+
+      it "returns 404 if not the current account" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        post "/actors/#{other.username}/following", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 404 if not the current account" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        post "/actors/#{other.username}/following", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        post "/actors/#{actor.username}/foobar", headers, "iri=#{other.iri}"
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        post "/actors/#{actor.username}/foobar", headers, {iri: other.iri}.to_json
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 400 if other does not exist" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        post "/actors/#{actor.username}/following", headers, "iri=foobar"
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 400 if other does not exist" do
+        actor && other
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        post "/actors/#{actor.username}/following", headers, {iri: "foobar"}.to_json
+        expect(response.status_code).to eq(400)
+      end
+    end
+  end
+
+  describe "DELETE /actors/:username/:relationship/:id" do
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Accept" => "text/html"}
+      delete "/actors/0/following/0", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    it "returns 401 if not authorized" do
+      headers = HTTP::Headers{"Accept" => "application/json"}
+      delete "/actors/0/following/0", headers
+      expect(response.status_code).to eq(401)
+    end
+
+    context "when authorized" do
+      let(actor) do
+        ActivityPub::Actor.find(Global.account.not_nil!.iri)
+      end
+      let(other) do
+        ActivityPub::Actor.new(iri: "https://unknown/#{random_string}").save
+      end
+      let(relationship) do
+        Relationship::Social::Follow.new(
+          from_iri: actor.iri,
+          to_iri: other.iri
+        ).save
+      end
+
+      sign_in
+
+      it "deletes the relationship and redirects" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        expect{delete "/actors/#{actor.username}/following/#{relationship.id}", headers}.to change{Relationship.count}.by(-1)
+        expect(response.status_code).to eq(302)
+        expect(response.headers["Location"]).to eq("/actors/#{actor.username}/following")
+      end
+
+      it "deletes the relationship and redirects" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        expect{delete "/actors/#{actor.username}/following/#{relationship.id}", headers}.to change{Relationship.count}.by(-1)
+        expect(response.status_code).to eq(302)
+        expect(response.headers["Location"]).to eq("/actors/#{actor.username}/following")
+      end
+
+      it "returns 404 if not the current account" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        delete "/actors/#{other.username}/following/#{relationship.id}", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 404 if not the current account" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        delete "/actors/#{other.username}/following/#{relationship.id}", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        delete "/actors/#{actor.username}/foobar/#{relationship.id}", headers
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 400 if relationship type is not supported" do
+        relationship
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        delete "/actors/#{actor.username}/foobar/#{relationship.id}", headers
+        expect(response.status_code).to eq(400)
+      end
+
+      it "returns 404 if relationship does not belong to current account" do
+        relationship = Relationship::Social::Follow.new(from_iri: other.iri, to_iri: other.iri).save
+        headers = HTTP::Headers{"Accept" => "text/html"}
+        delete "/actors/#{actor.username}/following/#{relationship.id}", headers
+        expect(response.status_code).to eq(404)
+      end
+
+      it "returns 404 if relationship does not belong to current account" do
+        relationship = Relationship::Social::Follow.new(from_iri: other.iri, to_iri: other.iri).save
+        headers = HTTP::Headers{"Accept" => "application/json"}
+        delete "/actors/#{actor.username}/following/#{relationship.id}", headers
+        expect(response.status_code).to eq(404)
+      end
+    end
+  end
+end
---
+++ src/controllers/relationships.cr
@@ -0,0 +1,112 @@
+require "../framework"
+
+class RelationshipsController
+  include Balloon::Controller
+
+  private macro actor_relationship_path
+    "/actors/#{env.params.url["username"]}/#{env.params.url["relationship"]}"
+  end
+
+  get "/actors/:username/:relationship" do |env|
+    unless (actor = get_actor(env))
+      not_found
+    end
+    unless (related = get_related(actor, env))
+      bad_request
+    end
+
+    if accepts?("text/html")
+      env.response.content_type = "text/html"
+      render "src/views/relationships/index.html.ecr"
+    else
+      env.response.content_type = "application/json"
+      render "src/views/relationships/index.json.ecr"
+    end
+  end
+
+  post "/actors/:username/:relationship" do |env|
+    unless (actor = get_actor(env))
+      not_found
+    end
+    unless (other = get_other(env))
+      bad_request
+    end
+    unless (related = make_related(actor, other, env))
+      bad_request
+    end
+    unless related.valid?
+      bad_request
+    end
+
+    related.save
+
+    env.redirect actor_relationship_path
+  end
+
+  delete "/actors/:username/:relationship/:id" do |env|
+    unless (actor = get_actor(env))
+      not_found
+    end
+    unless (relationship = find_related(env))
+      bad_request
+    end
+    unless actor.iri.in?({relationship.from_iri, relationship.to_iri})
+      not_found
+    end
+
+    relationship.destroy
+
+    env.redirect actor_relationship_path
+  end
+
+  private def self.pagination_params(env)
+    {
+      env.params.query["page"]?.try(&.to_u16) || 0_u16,
+      env.params.query["size"]?.try(&.to_u16) || 10_u16
+    }
+  end
+
+  private def self.get_actor(env)
+    (account = env.current_account?) &&
+      (username = env.params.url["username"]?) &&
+      account.username == username &&
+      (actor = account.actor?)
+    actor
+  end
+
+  private def self.get_other(env)
+    (iri = env.params.body["iri"]? || env.params.json["iri"]?) &&
+      (other = ActivityPub::Actor.find?(iri.to_s))
+    other
+  end
+
+  private def self.get_related(actor, env)
+    case env.params.url["relationship"]?
+    when "following"
+      actor.all_following(*pagination_params(env))
+    when "followers"
+      actor.all_followers(*pagination_params(env))
+    else
+    end
+  end
+
+  private def self.make_related(actor, other, env)
+    case env.params.url["relationship"]?
+    when "following"
+      actor.follow(other)
+    when "followers"
+      other.follow(actor)
+    else
+    end
+  end
+
+  private def self.find_related(env)
+    case env.params.url["relationship"]?
+    when "following"
+      Relationship::Social::Follow.find(env.params.url["id"].to_i)
+    when "followers"
+      Relationship::Social::Follow.find(env.params.url["id"].to_i)
+    else
+    end
+  end
+end

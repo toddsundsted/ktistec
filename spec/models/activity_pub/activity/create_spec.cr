1b51d35a31e08053f4ce624a422cd606954ecdf3
Todd Sundsted <todd@sundsted.com>
2020-07-20 08:51:46 -0400

    Add `Create` activity.

---
+++ spec/models/activity_pub/activity/create_spec.cr
@@ -0,0 +1,20 @@
+require "../../../spec_helper"
+
+Spectator.describe ActivityPub::Activity::Create do
+  before_each { Balloon.database.exec "BEGIN TRANSACTION" }
+  after_each { Balloon.database.exec "ROLLBACK" }
+
+  subject { described_class.new(iri: "http://test.test/#{random_string}").save }
+
+  describe "#actor" do
+    it "returns an actor or actor subclass" do
+      expect(typeof(subject.actor)).to eq({{(ActivityPub::Actor.all_subclasses << ActivityPub::Actor).join("|").id}})
+    end
+  end
+
+  describe "#object" do
+    it "returns an object or object subclass" do
+      expect(typeof(subject.object)).to eq({{(ActivityPub::Object.all_subclasses << ActivityPub::Object).join("|").id}})
+    end
+  end
+end

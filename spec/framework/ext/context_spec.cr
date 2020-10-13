26fcd4bf63cc51a38c5245c34dc0a13f0f9926ea
Todd Sundsted <todd@sundsted.com>
2020-10-13 04:42:38 -0400

    Add session management into the HTTP server context.

---
+++ spec/framework/ext/context_spec.cr
@@ -0,0 +1,107 @@
+require "../../spec_helper"
+
+class FooBarController
+  include Ktistec::Controller
+
+  skip_auth ["/foo/bar/session"]
+
+  get "/foo/bar/session" do |env|
+    env.session.to_json
+  end
+end
+
+Spectator.describe HTTP::Server::Context do
+  setup_spec
+
+  it "creates a new session" do
+    expect{get "/foo/bar/session"}.to change{Session.count}.by(1)
+  end
+
+  it "returns the session token in a header" do
+    get "/foo/bar/session"
+    payload = Ktistec::JWT.decode(response.headers["X-Auth-Token"])
+    expect(payload["jti"]).not_to be_nil
+  end
+
+  it "returns the session token in a cookie" do
+    get "/foo/bar/session"
+    payload = Ktistec::JWT.decode(response.cookies["AuthToken"].value)
+    expect(payload["jti"]).not_to be_nil
+  end
+
+  let(payload) { {jti: session.session_key, iat: Time.utc} }
+  let(jwt) { Ktistec::JWT.encode(payload) }
+
+  context "anonymous session" do
+    let(session) { Session.new.save }
+
+    it "uses an existing session" do
+      get "/foo/bar/session", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+      expect(JSON.parse(response.body).dig("session_key")).to eq(session.session_key)
+    end
+
+    context "with expired token" do
+      let(payload) { {jti: session.session_key, iat: 1.year.ago} }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+
+    context "with invalid token" do
+      let(payload) { {jti: "invalid session key", iat: Time.utc} }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+
+    context "with new secret key" do
+      let(jwt) { Ktistec::JWT.encode(payload, "old secret key") }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Authorization" => "Bearer #{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+  end
+
+  context "authenticated session" do
+    let(account) { Account.new(random_string, random_string).save }
+    let(session) { Session.new(account).save }
+
+    it "uses an existing session" do
+      get "/foo/bar/session", HTTP::Headers{"Cookie" => "AuthToken=#{jwt}"}
+      expect(JSON.parse(response.body).dig("session_key")).to eq(session.session_key)
+    end
+
+    context "with expired token" do
+      let(payload) { {jti: session.session_key, iat: 1.year.ago} }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Cookie" => "AuthToken=#{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+
+    context "with invalid token" do
+      let(payload) { {jti: "invalid session key", iat: Time.utc} }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Cookie" => "AuthToken=#{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+
+    context "with new secret key" do
+      let(jwt) { Ktistec::JWT.encode(payload, "old secret key") }
+
+      it "creates a new session" do
+        get "/foo/bar/session", HTTP::Headers{"Cookie" => "AuthToken=#{jwt}"}
+        expect(JSON.parse(response.body).dig("session_key")).not_to eq(session.session_key)
+      end
+    end
+  end
+end
--- src/framework/ext/context.cr
+++ src/framework/ext/context.cr
@@ -1,4 +1,43 @@
 class HTTP::Server::Context
-  property? current_account : Account?
-  property? current_session : Session?
+  property! session : Session
+
+  delegate :account, :account?, :account=, to: session
+
+  def session
+    @session ||= find_session || new_session
+  end
+
+  private def find_session
+    if (jwt = check_authorization || check_cookie)
+      if (payload = Ktistec::JWT.decode(jwt))
+        unless Ktistec::JWT.expired?(payload)
+          if (session = Session.find(session_key: payload["jti"].as_s))
+            return session
+          end
+        end
+      end
+    end
+  rescue Ktistec::JWT::Error | Ktistec::Model::NotFound
+  end
+
+  private def new_session
+    session = Session.new.save
+    payload = {"jti" => session.session_key, "iat" => Time.utc}
+    jwt = Ktistec::JWT.encode(payload)
+    response.headers["X-Auth-Token"] = jwt
+    response.cookies["AuthToken"] = jwt
+    session
+  end
+
+  private def check_authorization
+    if value = request.headers["Authorization"]?
+      if value.starts_with?("Bearer ")
+        value.split(" ").last
+      end
+    end
+  end
+
+  private def check_cookie
+    request.cookies["AuthToken"]?.try(&.value)
+  end
 end

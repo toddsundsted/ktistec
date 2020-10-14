07ab25b3fd14036b4cf26bb5d7d9e30794c63282
Todd Sundsted <todd@sundsted.com>
2020-10-14 06:05:07 -0400

    Integrate Serdar Dogruyol's CSRF handler.

---
+++ spec/framework/csrf_spec.cr
@@ -0,0 +1,138 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2016 Serdar Dogruyol
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require "../spec_helper"
+
+def process_request_and_return_response(handler, request)
+  io = IO::Memory.new
+  response = HTTP::Server::Response.new(io)
+  context = HTTP::Server::Context.new(request, response)
+  handler.call(context)
+  response.close
+  {context, HTTP::Client::Response.from_io(io.rewind, decompress: false)}
+end
+
+Spectator.describe Ktistec::CSRF do
+  setup_spec
+
+  it "sends GETs to next handler" do
+    handler = described_class.new
+    request = HTTP::Request.new("GET", "/")
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(404)
+  end
+
+  it "blocks POSTs without the token" do
+    handler = described_class.new
+    request = HTTP::Request.new("POST", "/")
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+  end
+
+  it "allows POSTs with the correct token in FORM submit" do
+    handler = described_class.new
+    request = HTTP::Request.new("POST", "/",
+      body: "authenticity_token=cemal&hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded"})
+    context, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+
+    jwt = client_response.headers["X-Auth-Token"]
+    csrf = context.session.string("csrf")
+
+    handler = described_class.new
+    request = HTTP::Request.new("POST", "/",
+      body: "authenticity_token=#{csrf}&hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded",
+                             "Authorization" => "Bearer #{jwt}"})
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(404)
+  end
+
+  it "allows POSTs with the correct token in HTTP header" do
+    handler = described_class.new
+    request = HTTP::Request.new("POST", "/",
+      body: "hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded"})
+    context, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+
+    jwt = client_response.headers["X-Auth-Token"]
+    csrf = context.session.string("csrf")
+
+    handler = described_class.new
+    request = HTTP::Request.new("POST", "/",
+      body: "hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded",
+                             "Authorization" => "Bearer #{jwt}",
+                             "X-CSRF-Token" => csrf})
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(404)
+  end
+
+  it "allows POSTs to allowed route" do
+    handler = described_class.new(allowed_routes: ["/allowed"])
+    request = HTTP::Request.new("POST", "/allowed/",
+      body: "authenticity_token=cemal&hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded"})
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(404)
+  end
+
+  it "allows POSTs to allowed route using wildcards" do
+    handler = described_class.new(allowed_routes: ["/everything/*"])
+    request = HTTP::Request.new("POST", "/everything/here/and",
+      body: "authenticity_token=cemal&hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded"})
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(404)
+  end
+
+  it "does not allow POSTs to mismatched route using wildcards" do
+    handler = described_class.new(allowed_routes: ["/nothing/*"])
+    request = HTTP::Request.new("POST", "/something/",
+      body: "authenticity_token=cemal&hasan=lamec",
+      headers: HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded"})
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+  end
+
+  it "outputs error string" do
+    handler = described_class.new(error: "Oh no you have an error")
+    request = HTTP::Request.new("POST", "/")
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+    expect(client_response.body).to eq("Oh no you have an error")
+  end
+
+  it "calls an error proc with context" do
+    handler = described_class.new(error: ->myerrorhandler(HTTP::Server::Context))
+    request = HTTP::Request.new("POST", "/")
+    _, client_response = process_request_and_return_response(handler, request)
+    expect(client_response.status_code).to eq(403)
+    expect(client_response.body).to eq("Error from handler")
+  end
+end
+
+def myerrorhandler(context : HTTP::Server::Context)
+  "Error from handler"
+end
--- src/framework/csrf.cr
+++ src/framework/csrf.cr
@@ -1,7 +1,89 @@
-require "kemal-session"
-require "kemal-csrf"
+# The MIT License (MIT)
 
-add_handler CSRF.new(
-  allowed_routes: ["/actors/:username/inbox"],
-  header: "X-CSRF-Token"
-)
+# Copyright (c) 2016 Serdar Dogruyol
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+module Ktistec
+  # CSRF preventing middleware.
+  #
+  class CSRF < Kemal::Handler
+    def initialize(
+         @header = "X-CSRF-Token",
+         @allowed_routes = [] of String,
+         @allowed_methods = %w(GET HEAD OPTIONS TRACE),
+         @parameter_name = "authenticity_token",
+         @error : String | (HTTP::Server::Context -> String) = "Forbidden (CSRF)"
+       )
+      setup
+    end
+
+    def setup
+      @allowed_routes.each do |path|
+        class_name = {{@type.name}}
+        %w(GET HEAD OPTIONS TRACE PUT POST).each do |method|
+          @@exclude_routes_tree.add "#{class_name}/#{method.downcase}#{path}", "/#{method.downcase}#{path}"
+        end
+      end
+    end
+
+    def call(context)
+      return call_next(context) if exclude_match?(context)
+
+      unless context.session.string?("csrf")
+        csrf_token = Random::Secure.hex(16)
+        context.session.string("csrf", csrf_token)
+        context.response.cookies << HTTP::Cookie.new(
+          name: @parameter_name,
+          value: csrf_token,
+          expires: Time.local.to_utc + 1.hour,
+          http_only: false
+        )
+      end
+
+      return call_next(context) if @allowed_methods.includes?(context.request.method)
+
+      req = context.request
+      submitted_token = if req.headers[@header]?
+                          req.headers[@header]
+                        elsif context.params.body[@parameter_name]?
+                          context.params.body[@parameter_name]
+                        else
+                          "nothing"
+                        end
+      current_token = context.session.string("csrf")
+      if current_token == submitted_token
+        # reset the token so it can't be used again
+        # context.session.string("csrf", Random::Secure.hex(16))
+        return call_next(context)
+      else
+        context.response.status_code = 403
+        if (error = @error) && !error.is_a?(String)
+          context.response.print error.call(context)
+        else
+          context.response.print error
+        end
+      end
+    end
+  end
+
+  add_handler CSRF.new(
+    allowed_routes: ["/actors/:username/inbox"]
+  )
+end

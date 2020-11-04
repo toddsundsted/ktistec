53cca8be1552b20775ab9d3ea51e09fc394b68a6
Todd Sundsted <todd@sundsted.com>
2020-11-04 09:25:27 -0500

    Support @-paths for actors.

---
+++ spec/framework/rewrite_spec.cr
@@ -0,0 +1,29 @@
+require "../spec_helper"
+
+class FooBarController
+  include Ktistec::Controller
+
+  skip_auth ["/actors/foobar"]
+
+  get "/actors/foobar" do |env|
+    "gotcha"
+  end
+end
+
+Spectator.describe Ktistec::Rewrite do
+  describe "get /@foobar" do
+    it "rewrites the request" do
+      get "/@foobar"
+      expect(response.status_code).to eq(200)
+      expect(response.body).to eq("gotcha")
+    end
+  end
+
+  describe "get /%40foobar" do
+    it "rewrites the request" do
+      get "/%40foobar"
+      expect(response.status_code).to eq(200)
+      expect(response.body).to eq("gotcha")
+    end
+  end
+end
---
+++ src/framework/rewrite.cr
@@ -0,0 +1,15 @@
+require "./framework"
+
+module Ktistec
+  # Path rewriting middleware.
+  #
+  class Rewrite < Kemal::Handler
+    def call(env)
+      return call_next(env) unless /^\/(@|%40)([a-zA-Z0-9.~_-]+).*/ =~ env.request.path
+      env.request.path = "/actors/#{$2}"
+      call_next(env)
+    end
+  end
+
+  add_handler Rewrite.new
+end

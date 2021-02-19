1326c4fa175ad88ff47707b9375ae4772623bc5f
Todd Sundsted <todd@sundsted.com>
2021-02-19 09:47:18 -0500

    Add tag statistics table.

---
+++ src/database/migrations/000009-create-tag-statistics.cr
@@ -0,0 +1,20 @@
+require "../../framework/database"
+
+extend Ktistec::Database::Migration
+
+up do |db|
+  db.exec <<-STR
+    CREATE TABLE tag_statistics (
+      "type" varchar(99) NOT NULL,
+      "name" varchar(99) NOT NULL COLLATE NOCASE,
+      "count" integer,
+      PRIMARY KEY("type", "name")
+    ) WITHOUT ROWID;
+  STR
+end
+
+down do |db|
+  db.exec <<-STR
+    DROP TABLE tag_statistics;
+  STR
+end

70cfa5ece5d3f93211f9bb5963344e9f3fb58693
Todd Sundsted <todd@sundsted.com>
2021-03-28 13:32:36 -0400

    Clean up variable names in label and content partials.

--- src/views/view_helper.cr
+++ src/views/view_helper.cr
@@ -1,7 +1,7 @@
 require "../framework/controller"
 
 module Ktistec::ViewHelper
-  def object_partial(env, _object, _actor = _object.attributed_to, _author = _actor)
+  def object_partial(env, object, actor = object.attributed_to, author = actor)
     render "src/views/partials/object.html.slang"
   end
 end

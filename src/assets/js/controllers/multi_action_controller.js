13e29f13b11aebcf5942396a9f079c6171048bcd
Todd Sundsted <todd@sundsted.com>
2021-04-16 20:12:06 -0400

    Update multi action controller.

--- src/assets/js/controllers/multi_action_controller.js
+++ src/assets/js/controllers/multi_action_controller.js
@@ -9,13 +9,23 @@ export default class extends Controller {
     event.preventDefault()
     let input = event.target
     let form = input.closest("form")
+    let action = input.getAttribute("action") || form.getAttribute("action")
+    let method = form.getAttribute("method")
     let data = new FormData(form)
     let xhr = new XMLHttpRequest()
-    xhr.open(form.getAttribute("method"), input.getAttribute("action"))
+    xhr.open(method, action)
     xhr.setRequestHeader("Accept", "text/html")
     xhr.onload = function() {
-      let location = xhr.responseURL
-      Turbolinks.visit(location || window.location)
+      let status = xhr.status
+      // detect redirect
+      if (xhr.responseURL && (new URL(xhr.responseURL).pathname != action)) {
+        Turbolinks.visit(xhr.responseURL)
+      } else {
+        let dom = new DOMParser().parseFromString(xhr.response, "text/html")
+        let new_form = dom.getElementById(form.id)
+        form.replaceWith(new_form)
+        scrollTo(0, 0)
+      }
     }
     xhr.send(data)
   }

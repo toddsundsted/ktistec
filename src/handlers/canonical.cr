ae6f565f1e3ad639fe46f5e40c1aade9083f2eae
Todd Sundsted <todd@sundsted.com>
2021-04-25 09:18:25 -0400

    Extend canonical handler to handle path suffixes like "thread", etc.

--- src/handlers/canonical.cr
+++ src/handlers/canonical.cr
@@ -9,15 +9,21 @@ module Ktistec::Handler
   class Canonical < Kemal::Handler
     include Ktistec::Controller
 
+    SUFFIXES = %w[thread]
+
     def call(env)
       return call_next(env) unless env.request.method == "GET"
-
       path = env.request.path
+      suffix = SUFFIXES.find { |suffix| path.ends_with?("/#{suffix}") }
+      if suffix
+        segment = "/#{suffix}"
+        path = path.chomp(segment)
+      end
       if (canonical = Relationship::Content::Canonical.find?(to_iri: path)) && accepts?("text/html") && !xhr?
         env.response.headers.add("Cache-Control", "max-age=3600")
-        env.redirect canonical.from_iri, 301
+        env.redirect "#{canonical.from_iri}#{segment}", 301
       elsif (canonical = Relationship::Content::Canonical.find?(from_iri: path))
-        env.request.path = canonical.to_iri
+        env.request.path = "#{canonical.to_iri}#{segment}"
         call_next(env)
       else
         call_next(env)

eb740e155017e65899f5c45a7dc9379caee7a0ad
Todd Sundsted <todd@sundsted.com>
2021-05-14 09:14:28 -0400

    Add `Timeline` relationship.

---
+++ spec/models/relationship/content/timeline_spec.cr
@@ -0,0 +1,41 @@
+require "../../../../src/models/relationship/content/timeline"
+
+require "../../../spec_helper/model"
+
+Spectator.describe Relationship::Content::Timeline do
+  setup_spec
+
+  let(options) do
+    {
+      from_iri: ActivityPub::Actor.new(iri: "https://test.test/#{random_string}").save.iri,
+      to_iri: ActivityPub::Object.new(iri: "https://test.test/#{random_string}").save.iri
+    }
+  end
+
+  context "creation" do
+    let(relationship) { described_class.new(**options).save }
+
+    it "creates confirmed relationships by default" do
+      expect(relationship.confirmed).to be_true
+    end
+  end
+
+  context "validation" do
+    it "rejects missing owner" do
+      new_relationship = described_class.new(**options.merge({from_iri: "missing"}))
+      expect(new_relationship.valid?).to be_false
+      expect(new_relationship.errors.keys).to contain("owner")
+    end
+
+    it "rejects missing object" do
+      new_relationship = described_class.new(**options.merge({to_iri: "missing"}))
+      expect(new_relationship.valid?).to be_false
+      expect(new_relationship.errors.keys).to contain("object")
+    end
+
+    it "successfully validates instance" do
+      new_relationship = described_class.new(**options)
+      expect(new_relationship.valid?).to be_true
+    end
+  end
+end

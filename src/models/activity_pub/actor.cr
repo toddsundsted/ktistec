d7cf6159f7d022afbb85d0b76327cbf36b59104c
Todd Sundsted <todd@sundsted.com>
2021-05-22 11:58:05 -0400

    Cleanup.

--- src/models/activity_pub/actor.cr
+++ src/models/activity_pub/actor.cr
@@ -570,7 +570,7 @@ module ActivityPub
       self.assign(**self.class.map(json, include_key: include_key))
     end
 
-    def self.map(json, *, include_key = false, **option)
+    def self.map(json, *, include_key = false, **options)
       json = Ktistec::JSON_LD.expand(JSON.parse(json)) if json.is_a?(String | IO)
       {
         iri: json.dig?("@id").try(&.as_s),

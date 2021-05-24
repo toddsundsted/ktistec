b11b68fa8c22731e606a1280aae0dd83b019516b
Todd Sundsted <todd@sundsted.com>
2021-05-24 08:03:52 -0400

    For clarity, refactor out the signature header check.

--- src/controllers/inboxes.cr
+++ src/controllers/inboxes.cr
@@ -47,8 +47,10 @@ class RelationshipsController
 
     # 1
 
+    signature = !!env.request.headers["Signature"]?
+
     if (actor_iri = activity.actor_iri)
-      unless (actor = ActivityPub::Actor.find?(actor_iri)) && (!env.request.headers["Signature"]? || actor.pem_public_key)
+      unless (actor = ActivityPub::Actor.find?(actor_iri)) && (!signature || actor.pem_public_key)
         open?(actor_iri) do |response|
           actor = ActivityPub::Actor.from_json_ld?(response.body, include_key: true).try(&.save)
         end
@@ -67,7 +69,7 @@ class RelationshipsController
 
     # 2
 
-    if env.request.headers["Signature"]?
+    if signature
       if actor && Ktistec::Signature.verify?(actor, "#{host}#{env.request.path}", env.request.headers, body)
         verified = true
       end

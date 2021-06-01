b8f507ab7e505281d0c3606113fd063e40e024f7
Todd Sundsted <todd@sundsted.com>
2021-06-01 08:49:37 -0400

    Add `Point`.

---
+++ src/database/migrations/000011-create-points.cr
@@ -0,0 +1,24 @@
+require "../../framework/database"
+
+extend Ktistec::Database::Migration
+
+up do |db|
+  db.exec <<-STR
+    CREATE TABLE points (
+      "id" integer PRIMARY KEY AUTOINCREMENT,
+      "chart" varchar(63) NOT NULL,
+      "timestamp" datetime NOT NULL,
+      "value" integer NOT NULL
+    )
+  STR
+  db.exec <<-STR
+    CREATE INDEX idx_points_chart_timestamp
+      ON points (chart ASC, timestamp ASC)
+  STR
+end
+
+down do |db|
+  db.exec <<-STR
+    DROP TABLE points;
+  STR
+end
---
+++ src/models/point.cr
@@ -0,0 +1,45 @@
+require "../framework/model"
+require "../framework/model/**"
+
+# Data point.
+#
+class Point
+  include Ktistec::Model(Nil)
+
+  @[Persistent]
+  property chart : String
+
+  @[Persistent]
+  property timestamp : Time { Time.utc }
+
+  @[Persistent]
+  property value : Int32
+
+  def self.charts
+    query = <<-QUERY
+      SELECT DISTINCT chart
+        FROM points
+    QUERY
+    Ktistec.database.query_all(query, as: String)
+  end
+
+  def self.chart(chart, begin _begin = nil, end _end = nil)
+    query = <<-QUERY
+      SELECT #{Point.columns(prefix: "p")}
+        FROM points AS p
+       WHERE p.chart = ?
+        #{_begin ? "AND p.timestamp >= ?" : ""}
+        #{_end ? "AND p.timestamp <= ?" : ""}
+    ORDER BY p.timestamp ASC
+    QUERY
+    if _begin && _end
+      Point.query_all(query, chart, _begin, _end)
+    elsif _begin
+      Point.query_all(query, chart, _begin)
+    elsif _end
+      Point.query_all(query, chart, _end)
+    else
+      Point.query_all(query, chart)
+    end
+  end
+end

ca500a743807f799d5de39b383d7586a634fb316
Todd Sundsted <todd@sundsted.com>
2021-07-24 09:43:22 -0400

    Test for association between account and actor.

--- spec/controllers/home_spec.cr
+++ spec/controllers/home_spec.cr
@@ -149,32 +149,58 @@ Spectator.describe HomeController do
         expect(JSON.parse(response.body)["errors"].as_h).to eq({"username" => ["is too short"], "password" => ["is too short"]})
       end
 
-      it "creates account and redirects" do
+      it "redirects and sets cookie" do
         headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
         body = "username=#{username}&password=#{password}&name=&summary="
-        expect{post "/", headers, body}.to change{Account.count}.by(1)
+        post "/", headers, body
         expect(response.status_code).to eq(302)
         expect(response.headers["Set-Cookie"]).to be_truthy
       end
 
-      it "also creates actor" do
+      it "creates account" do
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        body = "username=#{username}&password=#{password}&name=&summary="
+        expect{post "/", headers, body}.to change{Account.count}.by(1)
+      end
+
+      it "creates actor" do
         headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
         body = "username=#{username}&password=#{password}&name=&summary="
         expect{post "/", headers, body}.to change{ActivityPub::Actor.count}.by(1)
+      end
+
+      it "associates account and actor" do
+        headers = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
+        body = "username=#{username}&password=#{password}&name=&summary="
+        post "/", headers, body
+        expect(Account.find(username: username).actor).not_to be_nil
+      end
+
+      it "returns token" do
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        body = {username: username, password: password, name: "", summary: ""}.to_json
+        post "/", headers, body
+        expect(response.status_code).to eq(200)
+        expect(JSON.parse(response.body)["jwt"]).to be_truthy
       end
 
       it "creates account" do
         headers = HTTP::Headers{"Content-Type" => "application/json"}
         body = {username: username, password: password, name: "", summary: ""}.to_json
         expect{post "/", headers, body}.to change{Account.count}.by(1)
-        expect(response.status_code).to eq(200)
-        expect(JSON.parse(response.body)["jwt"]).to be_truthy
       end
 
-      it "also creates actor" do
+      it "creates actor" do
         headers = HTTP::Headers{"Content-Type" => "application/json"}
         body = {username: username, password: password, name: "", summary: ""}.to_json
         expect{post "/", headers, body}.to change{ActivityPub::Actor.count}.by(1)
+      end
+
+      it "associates account and actor" do
+        headers = HTTP::Headers{"Content-Type" => "application/json"}
+        body = {username: username, password: password, name: "", summary: ""}.to_json
+        post "/", headers, body
+        expect(Account.find(username: username).actor).not_to be_nil
       end
     end
   end

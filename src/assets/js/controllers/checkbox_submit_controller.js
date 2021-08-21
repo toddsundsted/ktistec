46eca834bed30fbb3a066459211e206c617ff014
Todd Sundsted <todd@sundsted.com>
2021-08-21 15:13:14 -0400

    Use Turbo frames for checkbox buttons.

---
+++ src/assets/js/controllers/checkbox_submit_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+
+/**
+ * Submit form when checkbox toggles.
+ *
+ * Wiring the change event directly to the form submit method does not
+ * work with Turbo--Turbo doesn't intercept it. Instead, create a
+ * hidden submit button and click that.
+ */
+export default class extends Controller {
+  connect() {
+    let input = document.createElement("input")
+    input.type = "submit"
+    input.style.display = "none"
+    this.element.appendChild(input)
+    this.input = input
+  }
+
+  change(event) {
+    this.input.click()
+  }
+}

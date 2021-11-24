bfe30204353e4856d1b07f2200d4c058306cce1f
Todd Sundsted <todd@sundsted.com>
2021-11-24 07:02:56 -0500

    Specify correct content type when signing activities to transfer.

--- src/framework/constants.cr
+++ src/framework/constants.cr
@@ -1,5 +1,7 @@
 module Ktistec
   module Constants
     PUBLIC = "https://www.w3.org/ns/activitystreams#Public"
+
+    ACTIVITY_STREAMS_CONTENT_TYPE = %q|application/ld+json; profile="https://www.w3.org/ns/activitystreams"|
   end
 end

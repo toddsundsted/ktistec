f38f87da6bf196a720616b9ecd99b888cf9adee1
Todd Sundsted <todd@sundsted.com>
2021-12-16 07:46:09 -0500

    Ensure mapped keys work for both `@id` and `@type`.

--- spec/framework/json_ld_spec.cr
+++ spec/framework/json_ld_spec.cr
@@ -252,6 +252,7 @@ Spectator.describe Ktistec::JSON_LD do
                 }
               }
             ],
+            "id": "https://id",
             "type": "Lock"
           }
         JSON
@@ -260,7 +261,8 @@ Spectator.describe Ktistec::JSON_LD do
 
     describe "#[]" do
       it "returns mapped terms" do
-        expect(json.as_h.keys).to match_array(["@context", "@type"]).in_any_order
+        expect(json.as_h.keys).to match_array(["@context", "@id", "@type"]).in_any_order
+        expect(json["@id"]).to eq("https://id")
         expect(json["@type"]).to eq("https://lock")
       end
     end

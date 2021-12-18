f8e045cdf51bcec68d99943c8c9b3eb19098c63f
Todd Sundsted <todd@sundsted.com>
2021-12-18 08:06:20 -0500

    To make it clear which action was invoked, just return a string.

--- spec/framework/method_spec.cr
+++ spec/framework/method_spec.cr
@@ -5,33 +5,33 @@ require "../spec_helper/controller"
 class FooBarController
   include Ktistec::Controller
 
-  skip_auth ["/foo/bar/delete/:id"], DELETE, POST
+  skip_auth ["/foo/bar/delete"], DELETE, POST
 
-  delete "/foo/bar/delete/:id" do |env|
+  delete "/foo/bar/delete" do |env|
     env.response.status_code = 410
-    env.params.url["id"]
+    "delete"
   end
 
-  post "/foo/bar/delete/:id" do |env|
+  post "/foo/bar/delete" do |env|
     env.response.status_code = 202
-    env.params.url["id"]
+    "post"
   end
 end
 
 Spectator.describe Ktistec::Method do
   HTML_HEADERS = HTTP::Headers{"Content-Type" => "application/x-www-form-urlencoded", "Accept" => "text/html"}
 
-  describe "post /foo/bar/delete/:id" do
+  describe "post /foo/bar/delete" do
     it "invokes the delete action" do
-      post "/foo/bar/delete/11", HTML_HEADERS, "_method=delete"
+      post "/foo/bar/delete", HTML_HEADERS, "_method=delete"
       expect(response.status_code).to eq(410)
-      expect(response.body).to eq("11")
+      expect(response.body).to eq("delete")
     end
 
     it "invokes the post action" do
-      post "/foo/bar/delete/13", HTML_HEADERS, ""
+      post "/foo/bar/delete", HTML_HEADERS, ""
       expect(response.status_code).to eq(202)
-      expect(response.body).to eq("13")
+      expect(response.body).to eq("post")
     end
   end
 end

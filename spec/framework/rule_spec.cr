2cc532481ff66161c9d07b0885b036bae299be92
Todd Sundsted <todd@sundsted.com>
2022-04-12 13:13:32 -0700

    Ktistec database specific rules framework.

---
+++ spec/framework/rule_spec.cr
@@ -0,0 +1,469 @@
+require "../../src/framework/rule"
+
+require "../spec_helper/model"
+
+class RuleModel
+  include Ktistec::Model(Nil)
+  include School::DomainType
+
+  @[Persistent]
+  property parent_id : Int64?
+  belongs_to child_of, class_name: RuleModel, foreign_key: parent_id, primary_key: id
+end
+
+Spectator.describe Ktistec::Rule do
+  describe ".make_pattern" do
+    Ktistec::Rule.make_pattern(
+      RulePattern,
+      RuleModel
+    )
+
+    it "inherits from School::Pattern" do
+      expect(RulePattern < School::Pattern).to be_true
+    end
+  end
+
+  context "given a pattern class" do
+    Ktistec::Rule.make_pattern(
+      RulePattern,
+      RuleModel,
+      associations: [child_of],
+      properties: [id]
+    )
+
+    describe "#vars" do
+      it "returns the vars" do
+        expect(RulePattern.new.vars).to be_empty
+      end
+
+      it "returns the vars" do
+        expect(RulePattern.new(School::Lit.new("lit")).vars).to be_empty
+      end
+
+      it "returns the vars" do
+        expect(RulePattern.new(School::Var.new("var")).vars).to eq(["var"])
+      end
+
+      it "returns the vars" do
+        expect(RulePattern.new(id: School::Lit.new("lit")).vars).to be_empty
+      end
+
+      it "returns the vars" do
+        expect(RulePattern.new(id: School::Var.new("var")).vars).to eq(["var"])
+      end
+    end
+
+    context "and a database and models" do
+      before_each do
+        Ktistec.database.exec <<-SQL
+          CREATE TABLE rule_models (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            parent_id integer
+          )
+        SQL
+        Ktistec.database.exec <<-SQL
+          INSERT INTO rule_models (id, parent_id)
+          VALUES (1, null), (2, 1), (3, 2)
+        SQL
+      end
+      after_each do
+        Ktistec.database.exec "DROP TABLE rule_models"
+      end
+
+      let(model1) { RuleModel.find(1) }
+      let(model2) { RuleModel.find(2) }
+      let(model3) { RuleModel.find(3) }
+      let(model9999) { RuleModel.new(id: 9999_i64) }
+      let(model_nil) { RuleModel.new(id: nil) }
+
+      let(yields) { [] of School::Bindings }
+
+      let(block) { ->(bindings : School::Bindings){ yields << bindings } }
+
+      let(bindings) { School::Bindings.new }
+
+      let(empty) { School::Bindings.new }
+
+      describe "#match" do
+        context "with no arguments" do
+          subject { RulePattern.new }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(3)
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty, empty, empty])
+          end
+        end
+
+        context "with an undefined argument" do
+          subject { RulePattern.new(foo: School::Var.new("foo")) }
+
+          it "raises an error" do
+            expect{subject.match(bindings, &block)}.to raise_error(ArgumentError)
+          end
+        end
+
+        # target
+
+        context "with a lit target that matches a model" do
+          subject { RulePattern.new(School::Lit.new(model1)) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty])
+          end
+        end
+
+        context "with a lit target that does not match a model" do
+          subject { RulePattern.new(School::Lit.new(model9999)) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with a lit target that does not match a model" do
+          subject { RulePattern.new(School::Lit.new(model_nil)) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with an unbound var target" do
+          subject { RulePattern.new(School::Var.new("target")) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(3)
+          end
+
+          it "binds the target to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"target" => model1}, {"target" => model2}, {"target" => model3}])
+          end
+        end
+
+        context "with a bound var target that matches a model" do
+          let(bindings) { School::Bindings{"target" => model1} }
+
+          subject { RulePattern.new(School::Var.new("target")) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "binds the target to the match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"target" => model1}])
+          end
+        end
+
+        context "with a bound var target that does not match a model" do
+          let(bindings) { School::Bindings{"target" => model9999} }
+
+          subject { RulePattern.new(School::Var.new("target")) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with a bound var target that does not match a model" do
+          let(bindings) { School::Bindings{"target" => model_nil} }
+
+          subject { RulePattern.new(School::Var.new("target")) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with a not target" do
+          subject { RulePattern.new(School::Not.new(School::Lit.new(model1), name: "target")) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(2)
+          end
+
+          it "binds the target to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"target" => model2}, {"target" => model3}])
+          end
+        end
+
+        context "with a not target" do
+          subject { RulePattern.new(School::Not.new(School::Lit.new(model_nil))) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(3)
+          end
+
+          it "binds the target to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty, empty, empty])
+          end
+        end
+
+        context "with a within target" do
+          subject { RulePattern.new(School::Within.new(School::Lit.new(model1), School::Lit.new(model3), name: "target")) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(2)
+          end
+
+          it "binds the target to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"target" => model1}, {"target" => model3}])
+          end
+        end
+
+        context "with a within target" do
+          subject { RulePattern.new(School::Within.new(School::Lit.new(model_nil))) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        # associations
+
+        context "with a lit association that matches a model" do
+          subject { RulePattern.new(child_of: School::Lit.new(model1)) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty])
+          end
+        end
+
+        context "with a lit association that does not match a model" do
+          subject { RulePattern.new(child_of: School::Lit.new(model9999)) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with an unbound var association" do
+          subject { RulePattern.new(child_of: School::Var.new("parent")) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(2)
+          end
+
+          it "binds the association to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"parent" => model1}, {"parent" => model2}])
+          end
+        end
+
+        context "with a bound var association that matches a model" do
+          let(bindings) { School::Bindings{"parent" => model1} }
+
+          subject { RulePattern.new(child_of: School::Var.new("parent")) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "binds the match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"parent" => model1}])
+          end
+        end
+
+        context "with a bound var association that does not match a model" do
+          let(bindings) { School::Bindings{"parent" => model9999} }
+
+          subject { RulePattern.new(child_of: School::Var.new("parent")) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        # properties
+
+        context "with a lit property that matches a model value" do
+          subject { RulePattern.new(id: School::Lit.new(1_i64)) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty])
+          end
+        end
+
+        context "with a lit property that matches a model value through accessor" do
+          subject { RulePattern.new(id: School::Lit.new(model1).id) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([empty])
+          end
+        end
+
+        context "with a lit property that does not match a model value" do
+          subject { RulePattern.new(id: School::Lit.new(model9999).id) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with a lit property that does not match a model value" do
+          subject { RulePattern.new(id: School::Lit.new(model_nil).id) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with an unbound var property" do
+          subject { RulePattern.new(id: School::Var.new("id")) }
+
+          it "invokes the block once for each match" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(3)
+          end
+
+          it "binds the property value to each match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"id" => 1_i64}, {"id" => 2_i64}, {"id" => 3_i64}])
+          end
+        end
+
+        context "with a bound var property that matches a model value" do
+          let(bindings) { School::Bindings{"id" => 1_i64} }
+
+          subject { RulePattern.new(id: School::Var.new("id")) }
+
+          it "invokes the block once" do
+            expect{subject.match(bindings, &block)}.to change{yields.size}.by(1)
+          end
+
+          it "binds the match" do
+            subject.match(bindings, &block)
+            expect(yields).to eq([{"id" => 1_i64}])
+          end
+        end
+
+        context "with a bound var property that does not match a model value" do
+          let(bindings) { School::Bindings{"id" => 9999_i64} }
+
+          subject { RulePattern.new(id: School::Var.new("id")) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+
+        context "with a bound var property that does not match a model value" do
+          let(bindings) { School::Bindings{"id" => nil} }
+
+          subject { RulePattern.new(id: School::Var.new("id")) }
+
+          it "does not invoke the block" do
+            expect{subject.match(bindings, &block)}.not_to change{yields.size}
+          end
+
+          it "does not bind values" do
+            subject.match(bindings, &block)
+            expect(yields).to be_empty
+          end
+        end
+      end
+
+      describe ".assert" do
+        let(bindings) { School::Bindings{"id" => 9999_i64} }
+
+        it "creates an instance" do
+          expect{RulePattern.assert(bindings, id: School::Lit.new(9999_i64))}.to change{RuleModel.count(id: 9999_i64)}.by(1)
+        end
+
+        it "creates an instance" do
+          expect{RulePattern.assert(bindings, id: School::Var.new("id"))}.to change{RuleModel.count(id: 9999_i64)}.by(1)
+        end
+      end
+
+      describe ".retract" do
+        let(bindings) { School::Bindings{"id" => 1_i64} }
+
+        it "destroys an instance" do
+          expect{RulePattern.retract(bindings, id: School::Lit.new(1_i64))}.to change{RuleModel.count(id: 1_i64)}.by(-1)
+        end
+
+        it "destroys an instance" do
+          expect{RulePattern.retract(bindings, id: School::Var.new("id"))}.to change{RuleModel.count(id: 1_i64)}.by(-1)
+        end
+      end
+    end
+  end
+end
---
+++ src/framework/rule.cr
@@ -0,0 +1,256 @@
+require "school/rule/pattern"
+
+require "./model"
+
+module Ktistec
+  module Rule
+    # Makes a pattern class given a model class.
+    #
+    macro make_pattern(name, clazz, associations = nil, properties = nil)
+      class {{name.id}} < School::Pattern
+        @vars = [] of String
+
+        alias Supported = School::Lit | School::Var | School::Not | School::Within
+
+        @options = {} of Symbol => Supported
+
+        def initialize(@target : Supported? = nil)
+          if (target = @target).is_a?(School::Var)
+            @vars << target.name
+          end
+        end
+
+        def initialize(@target : Supported? = nil, **options : Supported)
+          if (target = @target).is_a?(School::Var)
+            @vars << target.name
+          end
+          options.each do |name, expression|
+            @vars << expression.name if expression.is_a?(School::Var)
+            @options[name] = expression
+          end
+        end
+
+        # :inherit:
+        def vars : Enumerable(String)
+          @vars
+        end
+
+        {% clazz = clazz.resolve %}
+
+        # :inherit:
+        def match(bindings : School::Bindings, &block : School::Bindings -> Nil) : Nil
+          keys = @options.keys
+          {% if associations %}
+            keys -= {{associations.map(&.id.symbolize)}}
+          {% end %}
+          {% if properties %}
+            keys -= {{properties.map(&.id.symbolize)}}
+          {% end %}
+
+          raise ArgumentError.new("invalid arguments: #{keys.join(",")}") unless keys.empty?
+
+          table_name = {{clazz.id}}.table_name
+          column_names = {{clazz.id}}.columns(table_name)
+
+          conditions = [] of String?
+
+          if (target = @target)
+            conditions << condition(table_name, "id", target, bindings) do |value|
+              if value.responds_to?(:id) && value.id
+                value.id
+              end
+            end
+          end
+
+          {% if associations %}
+            {% for association in associations %}
+              {% unless (method = clazz.methods.find { |d| d.name == "_association_#{association.id}" }) %}
+                {% raise "#{association.id} is not an association on #{clazz}" %}
+              {% end %}
+              {% definition = method.body %}
+              {% if definition[0] == :belongs_to %}
+                if @options.has_key?({{association.id.symbolize}})
+                  conditions << condition(table_name, {{definition[2].id.stringify}}, @options[{{association.id.symbolize}}], bindings) do |value|
+                    if value.responds_to?({{definition[1]}}) && value.{{definition[1].id}}
+                      value.{{definition[1].id}}
+                    end
+                  end
+                end
+              {% else %}
+                {% raise "#{definition[0]} associations are not supported" %}
+              {% end %}
+            {% end %}
+          {% end %}
+
+          {% if properties %}
+            {% for property in properties %}
+              if @options.has_key?({{property.id.symbolize}})
+                conditions << condition(table_name, {{property.id.stringify}}, @options[{{property.id.symbolize}}], bindings) do |value|
+                  value
+                end
+              end
+            {% end %}
+          {% end %}
+
+          {% if clazz < Model::Undoable %}
+            conditions << "#{table_name}.undone_at IS NULL" unless @options.has_key?(:undone_at)
+          {% end %}
+          {% if clazz < Model::Deletable %}
+            conditions << "#{table_name}.deleted_at IS NULL" unless @options.has_key?(:deleted_at)
+          {% end %}
+          {% if clazz < Model::Polymorphic %}
+            unless @options.has_key?(:type)
+              types = {{(clazz.all_subclasses << clazz).map(&.stringify.stringify).join(",")}}
+              conditions << "#{table_name}.type IN (#{types})"
+            end
+          {% end %}
+
+          query = <<-SQL
+          SELECT #{column_names} FROM #{table_name}
+          SQL
+          unless conditions.compact.empty?
+            query += " WHERE " + conditions.compact.join(" AND ")
+          end
+
+          {{clazz.id}}.sql(query).each do |model|
+            temporary = bindings.dup
+
+            if (target = @target) && (name = target.name?) && !temporary.has_key?(name)
+              temporary[name] = model
+            end
+
+            {% if associations %}
+              {% for association in associations %}
+                if @options.has_key?({{association.id.symbolize}})
+                  if (target = @options[{{association.id.symbolize}}]) && (name = target.name?) && !temporary.has_key?(name)
+                    if (value = model.{{association.id}}?(include_deleted: true, include_undone: true))
+                      temporary[name] = value
+                    end
+                  end
+                end
+              {% end %}
+            {% end %}
+
+            {% if properties %}
+              {% for property in properties %}
+                if @options.has_key?({{property.id.symbolize}})
+                  if (target = @options[{{property.id.symbolize}}]) && (name = target.name?) && !temporary.has_key?(name)
+                    if (value = model.{{property.id}})
+                      temporary[name] = value
+                    end
+                  end
+                end
+              {% end %}
+            {% end %}
+
+            yield temporary
+          end
+        end
+
+        private def condition(table, column, expression, bindings, &block)
+          case expression
+          when School::Lit
+            if (term = yield expression.target)
+              %Q|"#{table}"."#{column}" = #{term.inspect}|
+            else
+              %Q|"#{table}"."#{column}" IS NULL|
+            end
+          when School::Var
+            if bindings.has_key?(expression.name)
+              if (term = yield bindings[expression.name])
+                %Q|"#{table}"."#{column}" = #{term.inspect}|
+              else
+                %Q|"#{table}"."#{column}" IS NULL|
+              end
+            else
+              %Q|"#{table}"."#{column}" IS NOT NULL|
+            end
+          when School::Not
+            target = expression.target
+            case target
+            when School::Lit
+              if (term = yield target.target)
+                %Q|"#{table}"."#{column}" != #{term.inspect}|
+              else
+                %Q|"#{table}"."#{column}" IS NOT NULL|
+              end
+            when School::Var
+              if bindings.has_key?(target.name)
+                if (term = yield bindings[target.name])
+                  %Q|"#{table}"."#{column}" != #{term.inspect}|
+                else
+                  %Q|"#{table}"."#{column}" IS NOT NULL|
+                end
+              else
+                %Q|"#{table}"."#{column}" IS NULL|
+              end
+            end
+          when School::Within
+            # if within contains an unbound var it will
+            # effectively match anything. `wildcard` tracks
+            # that.
+            wildcard = false
+            values = [] of String
+            expression.targets.each do |target|
+              case target
+              when School::Lit
+                if (term = yield target.target)
+                  values << term.inspect
+                else
+                  values << "NULL"
+                end
+              when School::Var
+                if bindings.has_key?(target.name)
+                  if (term = yield bindings[target.name])
+                    values << term.inspect
+                  else
+                    values << "NULL"
+                  end
+                else
+                  wildcard = true
+                end
+              end
+            end
+            if wildcard
+              %Q|"#{table}"."#{column}" IS NOT NULL|
+            else
+              %Q|"#{table}"."#{column}" IN (#{values.join(",")})|
+            end
+
+          when School::Accessor
+            if (term = yield expression.call(bindings))
+              %Q|"#{table}"."#{column}" = #{term.inspect}|
+            else
+              %Q|"#{table}"."#{column}" IS NULL|
+            end
+          else
+            raise "#{expression.class} is unsupported"
+          end
+        end
+
+        def self.assert(bindings : School::Bindings, **options : Supported)
+          {{clazz}}.new(transform(bindings, **options)).save
+        end
+
+        def self.retract(bindings : School::Bindings, **options : Supported)
+          {{clazz}}.find(transform(bindings, **options)).destroy
+        end
+
+        private def self.transform(bindings, **options)
+          options.to_h.transform_keys do |key|
+            key.to_s
+          end.transform_values do |value|
+            case value
+            when School::Lit
+              value.target
+            when School::Var
+              bindings[value.name]
+            else
+              raise "#{value.class} is unsupported"
+            end
+          end
+        end
+      end
+    end
+  end
+end

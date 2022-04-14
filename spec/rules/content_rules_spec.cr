e1056e64317ea8466c415c42a746bf3339965b47
Todd Sundsted <todd@sundsted.com>
2022-04-14 12:52:16 -0700

    First cut at Ktistec rules for notifications and timeline.

--- spec/rules/content_rules_spec.cr
+++ spec/rules/content_rules_spec.cr
@@ -3,23 +3,504 @@ require "../../src/rules/content_rules"
 require "../spec_helper/factory"
 require "../spec_helper/model"
 
+alias Notification = Relationship::Content::Notification
+alias Timeline = Relationship::Content::Timeline
+
 Spectator.describe ContentRules do
   setup_spec
 
-  let_build(:actor)
-  let_build(:activity)
+  {% if flag?(:"school:metrics") %}
+    before_all do
+      School::Metrics.reset
+    end
+    after_all do
+      metrics = School::Metrics.metrics
+      puts
+      puts "runs:            #{metrics[:runs]}"
+      puts "rules:           #{metrics[:rules]}"
+      puts "conditions:      #{metrics[:conditions]}"
+      puts "conditions/run:  #{metrics[:conditions_per_run]}"
+      puts "conditions/rule: #{metrics[:conditions_per_rule]}"
+      puts "operations:      #{metrics[:operations]}"
+      puts "operations/run:  #{metrics[:operations_per_run]}"
+      puts "operations/rule: #{metrics[:operations_per_rule]}"
+      puts "runtime:         #{metrics[:runtime]}"
+    end
+  {% end %}
 
   describe ".new" do
-    it "creates an instance for a given actor and activity" do
-      expect(described_class.new(actor, activity)).to be_a(ContentRules)
+    it "creates an instance" do
+      expect(described_class.new).to be_a(ContentRules)
     end
   end
 
-  describe "#run" do
-    subject { described_class.new(actor, activity) }
+  let(owner) { register.actor }
 
-    it "runs the rules engine" do
-      expect{subject.run}.not_to raise_error
+  let_build(:actor, named: other)
+  let_build(:object, attributed_to: owner)
+  let_create(:create, actor: owner, object: object)
+  let_create(:announce, actor: owner, object: object)
+  let_create(:like, actor: owner, object: object)
+  let_create(:follow, actor: owner, object: owner)
+  let_create(:delete, actor: owner, object: object)
+  let_create(:undo, actor: owner)
+
+  subject { described_class.new }
+
+  # notifications
+
+  describe "#run" do
+    context "given no notifications" do
+      pre_condition { expect(owner.notifications).to be_empty }
+
+      it "does not add the create to the notifications" do
+        subject.run(owner, create)
+        expect(owner.notifications).to be_empty
+      end
+
+      it "adds the announce to the notifications" do
+        subject.run(owner, announce)
+        expect(owner.notifications).to eq([announce])
+      end
+
+      it "adds the like to the notifications" do
+        subject.run(owner, like)
+        expect(owner.notifications).to eq([like])
+      end
+
+      it "adds the follow to the notifications" do
+        subject.run(owner, follow)
+        expect(owner.notifications).to eq([follow])
+      end
+
+      context "object mentions the owner" do
+        before_each do
+          object.assign(mentions: [
+            Factory.build(:mention, name: owner.iri, href: owner.iri)
+          ])
+        end
+
+        it "adds the create to the notifications" do
+          subject.run(owner, create)
+          expect(owner.notifications).to eq([create])
+        end
+      end
+
+      context "object mentions another actor" do
+        before_each do
+          object.assign(mentions: [
+            Factory.build(:mention, name: other.iri, href: other.iri)
+          ])
+        end
+
+        it "does not add the create to the notifications" do
+          subject.run(owner, create)
+          expect(owner.notifications).to be_empty
+        end
+      end
+
+      context "another object mentions the owner" do
+        let_create!(:object, named: nil, mentions: [
+          Factory.build(:mention, name: owner.iri, href: owner.iri)
+        ])
+
+        it "does not add the create to the notifications" do
+          subject.run(owner, create)
+          expect(owner.notifications).to be_empty
+        end
+      end
+
+      context "object is attributed to another actor" do
+        before_each { object.assign(attributed_to: other) }
+
+        it "does not add the announce to the notifications" do
+          subject.run(owner, announce)
+          expect(owner.notifications).to be_empty
+        end
+
+        it "does not add the like to the notifications" do
+          subject.run(owner, like)
+          expect(owner.notifications).to be_empty
+        end
+
+        context "another object is attributed to the owner" do
+          let_create!(:object, named: nil, attributed_to: owner)
+
+          it "does not add the announce to the notifications" do
+            subject.run(owner, announce)
+            expect(owner.notifications).to be_empty
+          end
+
+          it "does not add the like to the notifications" do
+            subject.run(owner, like)
+            expect(owner.notifications).to be_empty
+          end
+        end
+      end
+
+      context "follow does not follow the owner" do
+        before_each { follow.assign(object: other).save }
+
+        it "does not add the follow to the notifications" do
+          subject.run(owner, follow)
+          expect(owner.notifications).to be_empty
+        end
+      end
+    end
+
+    context "given notifictions with create already added" do
+      before_each do
+        put_in_notifications(owner, create)
+      end
+
+      pre_condition { expect(owner.notifications).to eq([create]) }
+
+      it "does not add the create to the notifications" do
+        subject.run(owner, create)
+        expect(owner.notifications).to eq([create])
+      end
+
+      it "removes the create from the notifications" do
+        subject.run(owner, delete)
+        expect(Notification.where(from_iri: owner.iri)).to be_empty
+      end
+
+      context "and an unrelated delete" do
+        let_create(:delete, named: unrelated)
+
+        it "does not remove the create from the notifications" do
+          subject.run(owner, unrelated)
+          expect(owner.notifications).to eq([create])
+        end
+      end
+
+      context "and an unrelated undo" do
+        before_each { undo.assign(object: announce).save }
+
+        it "does not remove the create from the notifications" do
+          subject.run(owner, undo)
+          expect(owner.notifications).to eq([create])
+        end
+      end
+    end
+
+    context "given notifictions with announce already added" do
+      before_each do
+        undo.assign(object: announce).save
+        put_in_notifications(owner, announce)
+      end
+
+      pre_condition { expect(owner.notifications).to eq([announce]) }
+
+      it "does not add the announce to the notifications" do
+        subject.run(owner, announce)
+        expect(owner.notifications).to eq([announce])
+      end
+
+      it "removes the announce from the notifications" do
+        subject.run(owner, undo)
+        expect(Notification.where(from_iri: owner.iri)).to be_empty
+      end
+
+      it "does not remove the announce from the notifications" do
+        subject.run(owner, delete)
+        expect(owner.notifications).to eq([announce])
+      end
+    end
+
+    context "given notifictions with like already added" do
+      before_each do
+        undo.assign(object: like).save
+        put_in_notifications(owner, like)
+      end
+
+      pre_condition { expect(owner.notifications).to eq([like]) }
+
+      it "does not add the like to the notifications" do
+        subject.run(owner, like)
+        expect(owner.notifications).to eq([like])
+      end
+
+      it "removes the like from the notifications" do
+        subject.run(owner, undo)
+        expect(Notification.where(from_iri: owner.iri)).to be_empty
+      end
+
+      it "does not remove the like from the notifications" do
+        subject.run(owner, delete)
+        expect(owner.notifications).to eq([like])
+      end
+    end
+
+    context "given notifictions with follow already added" do
+      before_each do
+        undo.assign(object: follow).save
+        put_in_notifications(owner, follow)
+      end
+
+      pre_condition { expect(owner.notifications).to eq([follow]) }
+
+      it "does not add the follow to the notifications" do
+        subject.run(owner, follow)
+        expect(owner.notifications).to eq([follow])
+      end
+
+      it "removes the follow from the notifications" do
+        subject.run(owner, undo)
+        expect(Notification.where(from_iri: owner.iri)).to be_empty
+      end
+
+      it "does not remove the follow from the notifications" do
+        subject.run(owner, delete)
+        expect(owner.notifications).to eq([follow])
+      end
+    end
+
+    # currently, the following should never happen, but if the
+    # activity has been undone, remove the corresponding notification.
+
+    context "given notifications with an announce that has been undone" do
+      before_each do
+        undo.assign(object: announce).save
+        put_in_notifications(owner, announce)
+        announce.undo
+      end
+
+      pre_condition do
+        expect(Notification.where(from_iri: owner.iri)).not_to be_empty
+      end
+
+      # a copy without the associations
+      let(undo_fresh) { ActivityPub::Activity::Undo.find(undo.id) }
+
+      it "removes the announce from the notifications" do
+        subject.run(owner, undo_fresh)
+        expect(Notification.where(from_iri: owner.iri)).to be_empty
+      end
+    end
+  end
+
+  # timeline
+
+  describe "#run" do
+    context "given an empty timeline" do
+      pre_condition { expect(owner.timeline).to be_empty }
+
+      it "adds the object to the timeline" do
+        subject.run(owner, create)
+        expect(owner.timeline).to eq([object])
+      end
+
+      it "adds the object to the timeline" do
+        subject.run(owner, announce)
+        expect(owner.timeline).to eq([object])
+      end
+
+      context "object is a reply" do
+        let_build(:object, named: :original, attributed_to: owner)
+
+        before_each { object.in_reply_to = original }
+
+        it "does not add the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to be_empty
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+      end
+
+      context "another object is a reply" do
+        let_build(:object, named: :another, attributed_to: owner)
+
+        before_each { another.in_reply_to = object }
+
+        it "adds the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to eq([object])
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+      end
+
+      context "object mentions the owner" do
+        before_each do
+          object.assign(mentions: [
+            Factory.build(:mention, name: owner.iri, href: owner.iri)
+          ])
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to eq([object])
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+      end
+
+      context "object mentions the owner and another actor" do
+        before_each do
+          object.assign(mentions: [
+            Factory.build(:mention, name: owner.iri, href: owner.iri),
+            Factory.build(:mention, name: other.iri, href: other.iri)
+          ])
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to eq([object])
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+      end
+
+      context "object mentions another actor" do
+        before_each do
+          object.assign(mentions: [
+            Factory.build(:mention, name: other.iri, href: other.iri)
+          ])
+        end
+
+        it "does not add the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to be_empty
+        end
+
+        it "adds the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+      end
+    end
+
+    context "given a timeline with an object already added" do
+      before_each { put_in_timeline(owner, object) }
+
+      pre_condition { expect(owner.timeline).to eq([object]) }
+
+      context "and an associated create" do
+        before_each { create }
+
+        it "does not add the object to the timeline" do
+          subject.run(owner, create)
+          expect(owner.timeline).to eq([object])
+        end
+
+        it "removes the object from the timeline" do
+          subject.run(owner, delete)
+          expect(Timeline.where(from_iri: owner.iri)).to be_empty
+        end
+
+        context "and an unrelated delete" do
+          let_create(:delete, named: unrelated)
+
+          it "does not remove the object from the timeline" do
+            subject.run(owner, unrelated)
+            expect(owner.timeline).to eq([object])
+          end
+        end
+
+        context "and an unrelated undo" do
+          before_each { undo.assign(object: announce).save }
+
+          it "does not remove the object from the timeline" do
+            subject.run(owner, undo)
+            expect(owner.timeline).to eq([object])
+          end
+        end
+      end
+
+      context "and an associated announce" do
+        before_each { announce }
+
+        it "does not add the object to the timeline" do
+          subject.run(owner, announce)
+          expect(owner.timeline).to eq([object])
+        end
+
+        it "removes the object from the timeline" do
+          subject.run(owner, delete)
+          expect(Timeline.where(from_iri: owner.iri)).to be_empty
+        end
+
+        context "and a related undo" do
+          before_each { undo.assign(object: announce).save }
+
+          it "removes the object from the timeline" do
+            subject.run(owner, undo)
+            expect(Timeline.where(from_iri: owner.iri)).to be_empty
+          end
+
+          context "and another announce" do
+            let_create!(:announce, named: another, actor: owner, object: object)
+
+            it "does not remove the object from the timeline" do
+              subject.run(owner, undo)
+              expect(owner.timeline).to eq([object])
+            end
+
+            context "that has been undone" do
+              before_each { another.undo }
+
+              it "removes the object from the timeline" do
+                subject.run(owner, undo)
+                expect(Timeline.where(from_iri: owner.iri)).to be_empty
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context "given a timeline with another object already added" do
+      let_build(:object, named: another)
+
+      before_each { put_in_timeline(owner, another) }
+
+      pre_condition { expect(owner.timeline).to eq([another]) }
+
+      it "does not remove the object from the timeline" do
+        subject.run(owner, delete)
+        expect(owner.timeline).to eq([another])
+      end
+
+      it "does not remove the object from the timeline" do
+        subject.run(owner, undo)
+        expect(owner.timeline).to eq([another])
+      end
+    end
+
+    # currently, the following should never happen, but if the object
+    # has been deleted, remove the corresponding timeline entry.
+
+    context "given a timeline with an object that has been deleted" do
+      before_each do
+        put_in_timeline(owner, object)
+        object.delete
+      end
+
+      pre_condition do
+        expect(Timeline.where(from_iri: owner.iri)).not_to be_empty
+      end
+
+      # a copy without the associations
+      let(delete_fresh) { ActivityPub::Activity::Delete.find(delete.id) }
+
+      it "destroys the timeline entry" do
+        subject.run(owner, delete_fresh)
+        expect(Timeline.where(from_iri: owner.iri)).to be_empty
+      end
     end
   end
 end
--- src/rules/content_rules.cr
+++ src/rules/content_rules.cr
@@ -1,9 +1,157 @@
-require "school"
+require "school/domain/builder"
+
+require "../framework/rule"
+require "../models/activity_pub/activity"
+require "../models/activity_pub/actor"
+require "../models/activity_pub/object"
+
+# optionally count database operations.
+
+{% if flag?(:"school:metrics") %}
+  module DB
+    abstract class Statement
+      def_around_query_or_exec do |args|
+        School::Metrics.count_operation
+        yield
+      end
+    end
+  end
+{% end %}
 
 class ContentRules
-  def initialize(@actor : ActivityPub::Actor, @activity : ActivityPub::Activity)
+  # domain types
+
+  class ::ActivityPub::Activity include School::DomainType end
+  class ::ActivityPub::Actor include School::DomainType end
+  class ::ActivityPub::Object include School::DomainType end
+  class ::Relationship::Content::Notification include School::DomainType end
+  class ::Relationship::Content::Timeline include School::DomainType end
+  class ::Tag::Mention include School::DomainType end
+
+  # patterns and facts for the rules below
+
+  Ktistec::Rule.make_pattern(Object, ActivityPub::Object, associations: [:in_reply_to, :attributed_to])
+  Ktistec::Rule.make_pattern(Mention, Tag::Mention, associations: [subject], properties: [href])
+  Ktistec::Rule.make_pattern(CreateActivity, ActivityPub::Activity::Create, associations: [:object])
+  Ktistec::Rule.make_pattern(AnnounceActivity, ActivityPub::Activity::Announce, associations: [:object])
+  Ktistec::Rule.make_pattern(LikeActivity, ActivityPub::Activity::Like, associations: [:object])
+  Ktistec::Rule.make_pattern(FollowActivity, ActivityPub::Activity::Follow, associations: [:object])
+  Ktistec::Rule.make_pattern(DeleteActivity, ActivityPub::Activity::Delete, associations: [:object])
+  Ktistec::Rule.make_pattern(UndoActivity, ActivityPub::Activity::Undo, associations: [:object])
+  Ktistec::Rule.make_pattern(Notification, Relationship::Content::Notification, associations: [:owner, :activity])
+  Ktistec::Rule.make_pattern(Timeline, Relationship::Content::Timeline, associations: [:owner, :object])
+
+  class IsAddressedTo < School::Relationship(ActivityPub::Activity, ActivityPub::Actor) end
+
+  private getter domain : School::Domain do
+    School.domain do
+      # Notifications
+
+      rule "create" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition CreateActivity, var("activity"), object: var("object")
+        any Mention, var("mention"), subject: var("object"), href: var("actor").iri
+        none Notification, owner: var("actor"), activity: var("activity")
+        assert Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      rule "announce" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition AnnounceActivity, var("activity"), object: var("object")
+        condition Object, var("object"), attributed_to: var("actor")
+        none Notification, owner: var("actor"), activity: var("activity")
+        assert Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      rule "like" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition LikeActivity, var("activity"), object: var("object")
+        condition Object, var("object"), attributed_to: var("actor")
+        none Notification, owner: var("actor"), activity: var("activity")
+        assert Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      rule "follow" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition FollowActivity, var("activity"), object: var("actor")
+        none Notification, owner: var("actor"), activity: var("activity")
+        assert Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      rule "delete" do
+        condition var("delete"), IsAddressedTo, var("actor")
+        condition DeleteActivity, var("delete"), object: var("object")
+        condition CreateActivity, var("activity"), object: var("object")
+        any Notification, owner: var("actor"), activity: var("activity")
+        retract Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      rule "undo" do
+        condition var("undo"), IsAddressedTo, var("actor")
+        condition UndoActivity, var("undo"), object: var("activity")
+        any Notification, owner: var("actor"), activity: var("activity")
+        retract Notification, owner: var("actor"), activity: var("activity")
+      end
+
+      # Timeline
+
+      # the first two rules would be one rule if "or" was supported.
+      # notify if there are either no replies and no mentions, or the
+      # actor is mentioned.
+
+      rule "create" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition CreateActivity, var("activity"), object: var("object")
+        none Object, var("object"), in_reply_to: var("any")
+        none Mention, var("mention"), subject: var("object")
+        none Timeline, owner: var("actor"), object: var("object")
+        assert Timeline, owner: var("actor"), object: var("object")
+      end
+
+      rule "create" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition CreateActivity, var("activity"), object: var("object")
+        any Mention, var("mention"), subject: var("object"), href: var("actor").iri
+        none Timeline, owner: var("actor"), object: var("object")
+        assert Timeline, owner: var("actor"), object: var("object")
+      end
+
+      rule "announce" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition AnnounceActivity, var("activity"), object: var("object")
+        none Timeline, owner: var("actor"), object: var("object")
+        assert Timeline, owner: var("actor"), object: var("object")
+      end
+
+      rule "delete" do
+        condition var("activity"), IsAddressedTo, var("actor")
+        condition DeleteActivity, var("activity"), object: var("object")
+        any Timeline, owner: var("actor"), object: var("object")
+        retract Timeline, owner: var("actor"), object: var("object")
+      end
+
+      rule "undo" do
+        condition var("undo"), IsAddressedTo, var("actor")
+        condition UndoActivity, var("undo"), object: var("activity")
+        condition AnnounceActivity, var("activity"), object: var("object")
+        none CreateActivity, object: var("object")
+        none AnnounceActivity, not(var("activity")), object: var("object")
+        any Timeline, owner: var("actor"), object: var("object")
+        retract Timeline, owner: var("actor"), object: var("object")
+      end
+    end
   end
 
-  def run
+  def run(actor, activity)
+    School::Fact.clear!
+
+    raise "actor must be local" unless actor.local?
+
+    domain.assert(IsAddressedTo.new(activity, actor))
+
+    5.times do |i|
+      break if domain.run == School::Domain::Status::Completed
+      raise "iteration limit exceeded" if i >= 4
+    end
   end
 end

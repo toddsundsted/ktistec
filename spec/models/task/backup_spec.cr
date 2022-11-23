9013b505e56ed5bb1b92abcb438fd20a1686667d
Todd Sundsted <todd@sundsted.com>
2022-11-22 20:50:12 -0500

    Cleanup. Prefer `described_class`.

--- spec/models/task/backup_spec.cr
+++ spec/models/task/backup_spec.cr
@@ -7,14 +7,14 @@ Spectator.describe Task::Backup do
 
   describe ".schedule_unless_exists" do
     it "schedules a new task" do
-      expect{described_class.schedule_unless_exists}.to change{Task::Backup.count}.by(1)
+      expect{described_class.schedule_unless_exists}.to change{described_class.count}.by(1)
     end
 
     context "given an existing task" do
       before_each { described_class.new.schedule }
 
       it "does not schedule a new task" do
-        expect{described_class.schedule_unless_exists}.not_to change{Task::Backup.count}
+        expect{described_class.schedule_unless_exists}.not_to change{described_class.count}
       end
     end
   end
--- spec/models/task/update_metrics_spec.cr
+++ spec/models/task/update_metrics_spec.cr
@@ -26,14 +26,14 @@ Spectator.describe Task::UpdateMetrics do
 
   describe ".schedule_unless_exists" do
     it "schedules a new task" do
-      expect{described_class.schedule_unless_exists}.to change{Task::UpdateMetrics.count}.by(1)
+      expect{described_class.schedule_unless_exists}.to change{described_class.count}.by(1)
     end
 
     context "given an existing task" do
       before_each { described_class.new.schedule }
 
       it "does not schedule a new task" do
-        expect{described_class.schedule_unless_exists}.not_to change{Task::UpdateMetrics.count}
+        expect{described_class.schedule_unless_exists}.not_to change{described_class.count}
       end
     end
   end

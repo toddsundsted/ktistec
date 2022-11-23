0bd438072d653c30f64c4fb001fa26e5fcebd661
Todd Sundsted <todd@sundsted.com>
2022-11-22 20:36:59 -0500

    Start `Performance` task.

--- src/ktistec/server.cr
+++ src/ktistec/server.cr
@@ -7,8 +7,9 @@ require "../workers/**"
 Ktistec::Server.run do
   Log.setup_from_env
   spawn { TaskWorker.start }
-  Task::UpdateMetrics.schedule_unless_exists
   Task::Backup.schedule_unless_exists
+  Task::Performance.schedule_unless_exists
+  Task::UpdateMetrics.schedule_unless_exists
   Session.clean_up_stale_sessions
   # check the rules when the server starts
   ContentRules.domain

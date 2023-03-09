4eda4da02b5e3f708ef081ac7bfd95298aad2287
Todd Sundsted <todd@sundsted.com>
2023-03-09 08:09:43 -0500

    Add `Tag::Mention.all_objects` method.

--- spec/models/tag/mention_spec.cr
+++ spec/models/tag/mention_spec.cr
@@ -1,6 +1,7 @@
 require "../../../src/models/tag/mention"
 
 require "../../spec_helper/base"
+require "../../spec_helper/factory"
 
 Spectator.describe Tag::Mention do
   setup_spec
@@ -10,6 +11,72 @@ Spectator.describe Tag::Mention do
       new_tag = described_class.new(subject_iri: "missing", name: "missing")
       expect(new_tag.valid?).to be_false
       expect(new_tag.errors.keys).to contain("subject")
+    end
+  end
+
+  describe ".all_objects" do
+    let_build(:actor, named: :author)
+
+    macro create_object_with_mentions(index, *mentions)
+      let_create!(
+        :object, named: object{{index}},
+        attributed_to: author,
+        published: Time.utc(2016, 2, 15, 10, 20, {{index}})
+      )
+      before_each do
+        {% for mention in mentions %}
+          described_class.new(
+            name: {{mention}},
+            subject: object{{index}}
+          ).save
+        {% end %}
+      end
+    end
+
+    create_object_with_mentions(1, "foo@remote", "bar@remote")
+    create_object_with_mentions(2, "foo@remote")
+    create_object_with_mentions(3, "foo@remote", "bar@remote")
+    create_object_with_mentions(4, "foo@remote")
+    create_object_with_mentions(5, "foo@remote", "quux@remote")
+
+    it "returns objects with the mention" do
+      expect(described_class.all_objects("bar@remote")).to eq([object3, object1])
+    end
+
+    it "filters out draft objects" do
+      object5.assign(published: nil).save
+      expect(described_class.all_objects("foo@remote")).not_to have(object5)
+    end
+
+    it "filters out deleted objects" do
+      object5.delete
+      expect(described_class.all_objects("foo@remote")).not_to have(object5)
+    end
+
+    it "filters out blocked objects" do
+      object5.block
+      expect(described_class.all_objects("foo@remote")).not_to have(object5)
+    end
+
+    it "filters out objects with deleted attributed to actors" do
+      author.delete
+      expect(described_class.all_objects("foo@remote")).to be_empty
+    end
+
+    it "filters out objects with blocked attributed to actors" do
+      author.block
+      expect(described_class.all_objects("foo@remote")).to be_empty
+    end
+
+    it "filters out objects with destroyed attributed to actors" do
+      author.destroy
+      expect(described_class.all_objects("foo@remote")).to be_empty
+    end
+
+    it "paginates the results" do
+      expect(described_class.all_objects("foo@remote", 1, 2)).to eq([object5, object4])
+      expect(described_class.all_objects("foo@remote", 2, 2)).to eq([object3, object2])
+      expect(described_class.all_objects("foo@remote", 2, 2).more?).to be_true
     end
   end
 end
--- src/models/tag/mention.cr
+++ src/models/tag/mention.cr
@@ -1,9 +1,51 @@
 require "../tag"
-require "../activity_pub/actor"
+require "../activity_pub/object"
 
 class Tag
   class Mention < Tag
     belongs_to subject, class_name: ActivityPub::Object, foreign_key: subject_iri, primary_key: iri
     validates(subject) { "missing: #{subject_iri}" unless subject? }
+
+    # Returns the objects with the given mention.
+    #
+    # Includes private (not visible) objects.
+    #
+    def self.all_objects(name, page = 1, size = 10)
+      query = <<-QUERY
+        SELECT #{ActivityPub::Object.columns(prefix: "o")}
+          FROM objects AS o
+          JOIN tags AS t
+            ON t.subject_iri = o.iri
+           AND t.type = "#{Tag::Mention}"
+          JOIN actors AS a
+            ON a.iri = o.attributed_to_iri
+         WHERE t.name = ?
+           AND o.published IS NOT NULL
+           AND o.deleted_at IS NULL
+           AND o.blocked_at IS NULL
+           AND a.deleted_at IS NULL
+           AND a.blocked_at IS NULL
+           AND o.id NOT IN (
+              SELECT o.id
+                FROM objects AS o
+                JOIN tags AS t
+                  ON t.subject_iri = o.iri
+                 AND t.type = "#{Tag::Mention}"
+                JOIN actors AS a
+                  ON a.iri = o.attributed_to_iri
+               WHERE t.name = ?
+                 AND o.published IS NOT NULL
+                 AND o.deleted_at IS NULL
+                 AND o.blocked_at IS NULL
+                 AND a.deleted_at IS NULL
+                 AND a.blocked_at IS NULL
+            ORDER BY o.published DESC
+               LIMIT ?
+           )
+      ORDER BY o.published DESC
+         LIMIT ?
+      QUERY
+      ActivityPub::Object.query_and_paginate(query, name, name, page: page, size: size)
+    end
   end
 end

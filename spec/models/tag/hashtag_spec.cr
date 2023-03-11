8a9622a35c7909b04e5dd79696c80f4c8b950a3d
Todd Sundsted <todd@sundsted.com>
2023-03-11 08:13:10 -0500

    Remove `Actor` as the subject of a hashtag.
    This was possible, but unused/unsupported in Ktistec, and not present
    in practice elsewhere, so far as I was able to discern.

--- spec/models/tag/hashtag_spec.cr
+++ spec/models/tag/hashtag_spec.cr
@@ -6,21 +6,6 @@ require "../../spec_helper/factory"
 Spectator.describe Tag::Hashtag do
   setup_spec
 
-  context "creation" do
-    let_build(:actor)
-    let_build(:object)
-
-    it "associates with an actor" do
-      expect{described_class.new(subject: actor, name: "actor").save}.to change{Tag::Hashtag.count}.by(1)
-      expect(described_class.find(name: "actor").subject).to eq(actor)
-    end
-
-    it "associates with an object" do
-      expect{described_class.new(subject: object, name: "object").save}.to change{Tag::Hashtag.count}.by(1)
-      expect(described_class.find(name: "object").subject).to eq(object)
-    end
-  end
-
   context "validation" do
     it "rejects missing subject" do
       new_tag = described_class.new(subject_iri: "missing", name: "missing")
--- src/models/tag/hashtag.cr
+++ src/models/tag/hashtag.cr
@@ -1,10 +1,9 @@
 require "../tag"
-require "../activity_pub/actor"
 require "../activity_pub/object"
 
 class Tag
   class Hashtag < Tag
-    belongs_to subject, class_name: ActivityPub::Object | ActivityPub::Actor, foreign_key: subject_iri, primary_key: iri
+    belongs_to subject, class_name: ActivityPub::Object, foreign_key: subject_iri, primary_key: iri
     validates(subject) { "missing: #{subject_iri}" unless subject? }
 
     # Returns the objects with the given tag.

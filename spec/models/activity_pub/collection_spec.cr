2992a2b16eb1408d4a2867b1ab25da223a24b33f
Todd Sundsted <todd@sundsted.com>
2023-04-10 20:45:35 -0400

    Enhance/complete the `Collection` class.

--- spec/models/activity_pub/collection_spec.cr
+++ spec/models/activity_pub/collection_spec.cr
@@ -3,6 +3,22 @@ require "../../../src/models/activity_pub/collection"
 require "../../spec_helper/base"
 
 Spectator.describe ActivityPub::Collection do
+  class ActivityPubModel
+    include Ktistec::Model(Nil)
+    include ActivityPub
+
+    @[Persistent]
+    property iri : String
+
+    def self.find?(iri, **options)
+      nil
+    end
+
+    def self.map(json, **options)
+      {"iri" => json.dig("@id").as_s}
+    end
+  end
+
   setup_spec
 
   context "when validating" do
@@ -24,6 +40,8 @@ Spectator.describe ActivityPub::Collection do
       expect(described_class.new(iri: "http://test.test/#{random_string}").save.valid?).to be_true
     end
   end
+
+  let(model) { ActivityPubModel.new(iri: "item link") }
 
   let(json) do
     <<-JSON
@@ -47,11 +65,11 @@ Spectator.describe ActivityPub::Collection do
       collection = described_class.from_json_ld(json).save
       expect(collection.iri).to eq("https://test.test/foo_bar")
       expect(collection.total_items).to eq(0)
-      expect(collection.first).to eq("first link")
-      expect(collection.last).to eq("last link")
-      expect(collection.prev).to eq("prev link")
-      expect(collection.next).to eq("next link")
-      expect(collection.current).to eq("current link")
+      expect(collection.first_iri).to eq("first link")
+      expect(collection.last_iri).to eq("last link")
+      expect(collection.prev_iri).to eq("prev link")
+      expect(collection.next_iri).to eq("next link")
+      expect(collection.current_iri).to eq("current link")
     end
 
     it "extracts items identifiers" do
@@ -76,6 +94,7 @@ Spectator.describe ActivityPub::Collection do
           "@id":"https://test.test/foo_bar",
           "items":[
             {
+              "type":"ActivityPubModel",
               "id":"item link"
             }
           ]
@@ -83,7 +102,7 @@ Spectator.describe ActivityPub::Collection do
       JSON
       collection = described_class.from_json_ld(json).save
       expect(collection.items_iris).to eq(["item link"])
-      expect(collection.items).to eq([{"@id" => "item link"}])
+      expect(collection.items).to eq([model])
     end
 
     it "handles ordered items" do
@@ -93,6 +112,7 @@ Spectator.describe ActivityPub::Collection do
           "@id":"https://test.test/foo_bar",
           "orderedItems":[
             {
+              "type":"ActivityPubModel",
               "id":"item link"
             }
           ]
@@ -100,7 +120,105 @@ Spectator.describe ActivityPub::Collection do
       JSON
       collection = described_class.from_json_ld(json).save
       expect(collection.items_iris).to eq(["item link"])
-      expect(collection.items).to eq([{"@id" => "item link"}])
+      expect(collection.items).to eq([model])
+    end
+
+    it "handles embedded first" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "first":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.from_json_ld(json).save
+      expect(collection.first).to be_a(ActivityPub::Collection)
+      expect(collection.first.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded last" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "last":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.from_json_ld(json).save
+      expect(collection.last).to be_a(ActivityPub::Collection)
+      expect(collection.last.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded prev" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "prev":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.from_json_ld(json).save
+      expect(collection.prev).to be_a(ActivityPub::Collection)
+      expect(collection.prev.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded next" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "next":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.from_json_ld(json).save
+      expect(collection.next).to be_a(ActivityPub::Collection)
+      expect(collection.next.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded current" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "current":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.from_json_ld(json).save
+      expect(collection.current).to be_a(ActivityPub::Collection)
+      expect(collection.current.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "raises an error" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "items":[
+            []
+          ]
+        }
+      JSON
+      expect{described_class.from_json_ld(json)}.to raise_error(TypeCastError)
     end
   end
 
@@ -109,11 +227,11 @@ Spectator.describe ActivityPub::Collection do
       collection = described_class.new.from_json_ld(json).save
       expect(collection.iri).to eq("https://test.test/foo_bar")
       expect(collection.total_items).to eq(0)
-      expect(collection.first).to eq("first link")
-      expect(collection.last).to eq("last link")
-      expect(collection.prev).to eq("prev link")
-      expect(collection.next).to eq("next link")
-      expect(collection.current).to eq("current link")
+      expect(collection.first_iri).to eq("first link")
+      expect(collection.last_iri).to eq("last link")
+      expect(collection.prev_iri).to eq("prev link")
+      expect(collection.next_iri).to eq("next link")
+      expect(collection.current_iri).to eq("current link")
     end
 
     it "extracts items identifiers" do
@@ -138,6 +256,7 @@ Spectator.describe ActivityPub::Collection do
           "@id":"https://test.test/foo_bar",
           "items":[
             {
+              "type":"ActivityPubModel",
               "id":"item link"
             }
           ]
@@ -145,7 +264,7 @@ Spectator.describe ActivityPub::Collection do
       JSON
       collection = described_class.new.from_json_ld(json).save
       expect(collection.items_iris).to eq(["item link"])
-      expect(collection.items).to eq([{"@id" => "item link"}])
+      expect(collection.items).to eq([model])
     end
 
     it "handles ordered items" do
@@ -155,6 +274,7 @@ Spectator.describe ActivityPub::Collection do
           "@id":"https://test.test/foo_bar",
           "orderedItems":[
             {
+              "type":"ActivityPubModel",
               "id":"item link"
             }
           ]
@@ -162,7 +282,105 @@ Spectator.describe ActivityPub::Collection do
       JSON
       collection = described_class.new.from_json_ld(json).save
       expect(collection.items_iris).to eq(["item link"])
-      expect(collection.items).to eq([{"@id" => "item link"}])
+      expect(collection.items).to eq([model])
+    end
+
+    it "handles embedded first" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "first":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.new.from_json_ld(json).save
+      expect(collection.first).to be_a(ActivityPub::Collection)
+      expect(collection.first.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded last" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "last":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.new.from_json_ld(json).save
+      expect(collection.last).to be_a(ActivityPub::Collection)
+      expect(collection.last.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded prev" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "prev":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.new.from_json_ld(json).save
+      expect(collection.prev).to be_a(ActivityPub::Collection)
+      expect(collection.prev.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded next" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "next":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.new.from_json_ld(json).save
+      expect(collection.next).to be_a(ActivityPub::Collection)
+      expect(collection.next.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "handles embedded current" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "current":{
+            "items":[
+              "item link"
+            ]
+          }
+        }
+      JSON
+      collection = described_class.new.from_json_ld(json).save
+      expect(collection.current).to be_a(ActivityPub::Collection)
+      expect(collection.current.not_nil!.items_iris).to eq(["item link"])
+    end
+
+    it "raises an error" do
+      json = <<-JSON
+        {
+          "@context":"https://www.w3.org/ns/activitystreams",
+          "@id":"https://test.test/foo_bar",
+          "items":[
+            []
+          ]
+        }
+      JSON
+      expect{described_class.new.from_json_ld(json)}.to raise_error(TypeCastError)
     end
   end
 
@@ -170,6 +388,53 @@ Spectator.describe ActivityPub::Collection do
     it "renders an identical instance" do
       collection = described_class.from_json_ld(json)
       expect(described_class.from_json_ld(collection.to_json_ld)).to eq(collection)
+    end
+
+    subject { described_class.new(iri: "https://remote/#{random_string}") }
+
+    it "embeds first" do
+      subject.first = described_class.new(iri: "https://remote/first")
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("first", "id")).to eq("https://remote/first")
+    end
+
+    it "embeds last" do
+      subject.last = described_class.new(iri: "https://remote/last")
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("last", "id")).to eq("https://remote/last")
+    end
+
+    it "embeds prev" do
+      subject.prev = described_class.new(iri: "https://remote/prev")
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("prev", "id")).to eq("https://remote/prev")
+    end
+
+    it "embeds next" do
+      subject.next = described_class.new(iri: "https://remote/next")
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("next", "id")).to eq("https://remote/next")
+    end
+
+    it "embeds current" do
+      subject.current = described_class.new(iri: "https://remote/current")
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("current", "id")).to eq("https://remote/current")
+    end
+
+    it "embeds local item" do
+      subject.items = [described_class.new(iri: "https://test.test/item").as(ActivityPub | String)]
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("items", 0, "id")).to eq("https://test.test/item")
+    end
+
+    it "links remote item" do
+      subject.items = [described_class.new(iri: "https://remote/item").as(ActivityPub | String)]
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("items", 0)).to eq("https://remote/item")
+    end
+
+    it "links item" do
+      subject.items = ["https://remote/item".as(ActivityPub | String)]
+      expect(JSON.parse(subject.to_json_ld(recursive: true)).dig("items", 0)).to eq("https://remote/item")
+    end
+
+    it "links item" do
+      subject.items_iris = ["https://remote/item"]
+      expect(JSON.parse(subject.to_json_ld).dig("items", 0)).to eq("https://remote/item")
     end
   end
 end

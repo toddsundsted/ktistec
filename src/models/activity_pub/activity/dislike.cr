11669357d3314f7ed0439190186ed8348abc3bba
Todd Sundsted <todd@sundsted.com>
2023-11-18 12:29:47 -0800

    Refactor common object functionality into superclass.
    Rules that operate on object activities are common.

--- src/models/activity_pub/activity/dislike.cr
+++ src/models/activity_pub/activity/dislike.cr
@@ -2,7 +2,6 @@ require "../activity"
 require "../object"
 
 class ActivityPub::Activity
-  class Dislike < ActivityPub::Activity
-    belongs_to object, class_name: ActivityPub::Object, foreign_key: object_iri, primary_key: iri
+  class Dislike < ActivityPub::Activity::ObjectActivity
   end
 end

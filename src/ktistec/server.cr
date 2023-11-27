c78b761e313555a398ca2698ce147ddc2416c042
Todd Sundsted <todd@sundsted.com>
2023-11-27 05:38:43 -0800

    Ensure tasks are created before the task worker starts.

--- src/ktistec/server.cr
+++ src/ktistec/server.cr
@@ -6,11 +6,15 @@ require "../workers/**"
 
 Ktistec::Server.run do
   Log.setup_from_env
-  spawn { TaskWorker.start }
-  Task::Backup.schedule_unless_exists
-  Task::Performance.schedule_unless_exists
-  Task::UpdateMetrics.schedule_unless_exists
-  Session.clean_up_stale_sessions
+  spawn do
+    TaskWorker.destroy_old_tasks
+    TaskWorker.clean_up_running_tasks
+    Task::Backup.schedule_unless_exists
+    Task::Performance.schedule_unless_exists
+    Task::UpdateMetrics.schedule_unless_exists
+    Session.clean_up_stale_sessions
+    TaskWorker.start
+  end
   # track server starts
   Point.new(chart: "server-start", timestamp: Time.utc, value: 1).save
   # check the rules when the server starts

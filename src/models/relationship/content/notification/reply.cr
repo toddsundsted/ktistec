53744f74fde5a3c05cba77dd13ea69987463f83e
Todd Sundsted <todd@sundsted.com>
2023-12-02 10:23:30 -0500

    Refactor `Notification` base class.
    1. Make `Notification` abstract.
    
    2. Remove `activity` property. Some subclasses associate with
       activities but others may associate with objects, for example.

--- src/models/relationship/content/notification/reply.cr
+++ src/models/relationship/content/notification/reply.cr
@@ -5,6 +5,8 @@ class Relationship
   class Content
     class Notification < Relationship
       class Reply < Notification
+        belongs_to activity, class_name: ActivityPub::Activity::Create, foreign_key: to_iri, primary_key: iri
+        validates(activity) { "missing: #{to_iri}" unless activity? }
       end
     end
   end

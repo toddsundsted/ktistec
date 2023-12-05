e81317cfc5b547d5ac41bfc50c86865d5fa962dc
Todd Sundsted <todd@sundsted.com>
2023-12-05 07:06:41 -0800

    Add `LastTime`.

---
+++ spec/models/last_time_spec.cr
@@ -0,0 +1,47 @@
+require "../../src/models/last_time"
+
+require "../spec_helper/base"
+require "../spec_helper/factory"
+
+Spectator.describe LastTime do
+  setup_spec
+
+  context "validations" do
+    let(account) { register }
+
+    let(options) do
+      {
+        name: random_string,
+        account_id: account.id
+      }
+    end
+
+    it "rejects blank name" do
+      new_last_time = described_class.new(**options.merge({name: ""}))
+      expect(new_last_time.valid?).to be_false
+      expect(new_last_time.errors.keys).to contain("name")
+    end
+
+    it "rejects duplicates" do
+      described_class.new(**options).save
+      new_last_time = described_class.new(**options)
+      expect(new_last_time.valid?).to be_false
+      expect(new_last_time.errors.keys).to contain("name")
+    end
+
+    it "rejects non-existent account" do
+      new_last_time = described_class.new(**options.merge({account_id: 0_i64}))
+      expect(new_last_time.valid?).to be_false
+      expect(new_last_time.errors.keys).to contain("account")
+    end
+
+    it "accepts nil account_id" do
+      new_last_time = described_class.new(**options.merge({account_id: nil}))
+      expect(new_last_time.valid?).to be_true
+    end
+
+    it "successfully validates instance" do
+      new_last_time = described_class.new(**options)
+    end
+  end
+end
---
+++ src/models/last_time.cr
@@ -0,0 +1,29 @@
+require "../framework/model"
+require "./account"
+
+# The last time something was visited, checked or changed.
+#
+class LastTime
+  include Ktistec::Model(Common)
+
+  @@table_name = "last_times"
+
+  @[Persistent]
+  @[Insignificant]
+  property timestamp : Time { Time.utc }
+
+  @[Persistent]
+  property name : String
+  validates(name) do
+    if !name.presence
+      "can't be blank"
+    elsif (instance = self.class.where(name: name, account_id: account_id).first?) && instance.id != self.id
+      "already exists: #{name}"
+    end
+  end
+
+  @[Persistent]
+  property account_id : Int64?
+  belongs_to account
+  validates(account) { "can't find Account with id=#{account_id}" if account_id && !account? }
+end

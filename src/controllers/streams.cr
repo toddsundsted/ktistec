586fcdb4d61950de430f889bc95186d86ea67dcf
Todd Sundsted <todd@sundsted.com>
2024-06-17 12:06:46 -0400

    Support "no-op" Turbo Streams actions.

--- src/controllers/streams.cr
+++ src/controllers/streams.cr
@@ -57,25 +57,33 @@ class StreamsController
     response.flush
   end
 
+  # Sends a no-op action.
+  #
+  def self.stream_no_op(io)
+    stream_action(io, nil, "no-op", nil, nil)
+  end
+
   {% for action in %w(append prepend replace update remove before after morph refresh) %}
-    def self.stream_{{action.id}}(io, body, id = nil, selector = nil)
+    def self.stream_{{action.id}}(io, body = nil, id = nil, selector = nil)
       stream_action(io, body, {{action}}, id, selector)
     end
   {% end %}
 
-  def self.stream_action(io : IO, body : String, action : String, id : String?, selector : String?)
+  def self.stream_action(io : IO, body : String?, action : String, id : String?, selector : String?)
     if id && !selector
       io.puts %Q|data: <turbo-stream action="#{action}" target="#{id}">|
     elsif selector && !id
       io.puts %Q|data: <turbo-stream action="#{action}" targets="#{selector}">|
     else
-      raise "one of `id` or `selector` must be provided"
+      io.puts %Q|data: <turbo-stream action="#{action}">|
     end
-    io.puts "data: <template>"
-    body.each_line do |line|
-      io.puts "data: #{line}"
+    if body
+      io.puts "data: <template>"
+      body.each_line do |line|
+        io.puts "data: #{line}"
+      end
+      io.puts "data: </template>"
     end
-    io.puts "data: </template>"
     io.puts "data: </turbo-stream>"
     io.puts
     io.flush

e3d37ef7027874ae6100af73e83a8c7c615892da
Todd Sundsted <todd@sundsted.com>
2024-12-27 05:01:10 -0800

    Only provide default language and local timezone values in setup.
    If these values weren't set in the database, providing values in the
    form was (convenient but) confusing because there was no clear
    indication that the form needed to be saved to persist them.

--- src/assets/js/controllers/default_language_controller.js
+++ src/assets/js/controllers/default_language_controller.js
@@ -3,8 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const language = navigator.language
-    if (!this.element.value && !this.element.closest(".field.error")) {
-      this.element.value = language
+    let placeholder = `IETF BCP 47 language tag. For example: ${language}`
+    let placeholderOnly = this.element.dataset["placeholder-only"]
+    this.element.setAttribute("placeholder", placeholder)
+    if (!placeholderOnly) {
+      if (!this.element.value && !this.element.closest(".field.error")) {
+        this.element.value = language
+      }
     }
   }
 }
--- src/assets/js/controllers/local_timezone_controller.js
+++ src/assets/js/controllers/local_timezone_controller.js
@@ -3,8 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    if (!this.element.value && !this.element.closest(".field.error")) {
-      this.element.value = timezone
+    let placeholder = `IANA time zone database identifier. For example: ${timezone}`
+    let placeholderOnly = this.element.dataset["placeholder-only"]
+    this.element.setAttribute("placeholder", placeholder)
+    if (!placeholderOnly) {
+      if (!this.element.value && !this.element.closest(".field.error")) {
+        this.element.value = timezone
+      }
     }
   }
 }

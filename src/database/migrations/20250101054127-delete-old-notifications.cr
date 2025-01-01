0582a8912af3733c1b54f1d83a080f33bfa1a973
Todd Sundsted <todd@sundsted.com>
2025-01-01 06:03:23 -0800

    Fix/replace migration.

--- src/database/migrations/20250101054127-delete-old-notifications.cr
+++ src/database/migrations/20250101054127-delete-old-notifications.cr
@@ -1,11 +1,18 @@
 require "../../framework/database"
 
+# Migration 20240212125448 was incorrect. It was also one way. Delete
+# the record of the old migration and rerun the intended migration.
+# See: https://github.com/toddsundsted/ktistec/commit/69e2fe66dfa28077438ae8cfe4afc85b3ee09b5c
+
 extend Ktistec::Database::Migration
 
 up do |db|
   db.exec <<-STR
+    DELETE FROM migrations WHERE id = 20240212125448;
+  STR
+  db.exec <<-STR
     DELETE FROM relationships
-    WHERE type IN ("Relationship::Content::Notification::Hashtag", "Relationship::Content::Notification::Threadx")
+    WHERE type IN ("Relationship::Content::Notification::Hashtag", "Relationship::Content::Notification::Thread")
   STR
   db.exec <<-STR
     DELETE FROM relationships

815c33f0919badf94cb6925761164d17ef036265
Todd Sundsted <todd@sundsted.com>
2025-07-26 22:51:59 -0400

    Add OAuth models.

---
+++ src/models/oauth2/provider/access_token.cr
@@ -0,0 +1,54 @@
+require "../../../framework/model"
+require "../../../framework/model/**"
+
+module OAuth2
+  module Provider
+    class AccessToken
+      include Ktistec::Model
+      include Ktistec::Model::Common
+
+      @@table_name = "oauth_access_tokens"
+
+      # The access token string.
+      #
+      # This is a unique, randomly generated, and unguessable value
+      # that the client application uses to authenticate its requests
+      # to the server. The client sends this token in the
+      # `Authorization` header of an API request.
+      #
+      @[Persistent]
+      property token : String
+
+      # The client application ID.
+      #
+      # Links the access token to the OAuth2 `Client` model. It
+      # identifies *which application* this token was issued to.
+      #
+      @[Persistent]
+      property client_id : Int64
+
+      # The account ID.
+      #
+      # Links the access token to the `Account` model. It identifies
+      # *which user* the token represents.
+      #
+      @[Persistent]
+      property account_id : Int64
+
+      # The expiration.
+      #
+      # A timestamp that indicates when the access token becomes invalid.
+      #
+      @[Persistent]
+      property expires_at : Time
+
+      # Scopes.
+      #
+      # Stores the specific permissions that the user granted when
+      # they authorized the application.
+      #
+      @[Persistent]
+      property scope : String
+    end
+  end
+end
---
+++ src/models/oauth2/provider/client.cr
@@ -0,0 +1,62 @@
+require "../../../framework/model"
+require "../../../framework/model/**"
+
+module OAuth2
+  module Provider
+    class Client
+      include Ktistec::Model
+      include Ktistec::Model::Common
+
+      @@table_name = "oauth_clients"
+
+      # Public identifier for a third-party application.
+      #
+      # When an application wants to request access to a user's
+      # account, it sends this ID to identify itself. It is not a
+      # secret.
+      #
+      @[Persistent]
+      property client_id : String
+
+      # Client secret.
+      #
+      # This is a confidential secret known only to the application
+      # and the Ktistec server. It's used clients to authenticate
+      # themselves when they exchange an authorization code for an
+      # access token. This proves that the request is coming from the
+      # legitimate application.
+      #
+      @[Persistent]
+      property client_secret : String
+
+      # A human-readable name for the application.
+      #
+      # The name is shown to the user on the consent screen so they
+      # know which application is asking for permission.
+      #
+      @[Persistent]
+      property client_name : String
+
+      # A list of redirect URIs.
+      #
+      # This is a critical security feature. It is a list of one or
+      # more URIs that the Ktistec server is allowed to redirect the
+      # user back to after they have authorized the application. The
+      # `redirect_uri` sent in the authorization request *must* be one
+      # of the URLs in this list.
+      #
+      @[Persistent]
+      property redirect_uris : String
+
+      # Scopes.
+      #
+      # Defines the specific permissions the application is requesting
+      # (e.g., `read`, `write`). The user will see these requested
+      # scopes on the consent screen and must approve them. The
+      # resulting access token will be limited to these permissions.
+      #
+      @[Persistent]
+      property scope : String
+    end
+  end
+end

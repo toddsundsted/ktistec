5ebd5b4a59ef7aa5eb9705cba62b8f41fe16b4a8
Todd Sundsted <todd@sundsted.com>
2025-09-21 06:38:03 -0700

    Only respond to clicks on the dropdown menu item.

--- src/assets/js/controllers/dropdown_controller.js
+++ src/assets/js/controllers/dropdown_controller.js
@@ -6,7 +6,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   click(event) {
     let menu = this.element.querySelector(".menu")
-    if (menu) {
+    if (menu && (event.target === this.element || !menu.contains(event.target))) {
       menu.style.display =
         menu.style.display ?
         null : "block"

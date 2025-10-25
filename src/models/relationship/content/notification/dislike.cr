f227de2c09d5b601d13ff7f16da3ac09945cdf1b
Todd Sundsted <todd@sundsted.com>
2025-10-25 07:58:27 -0700

    Add `Relationship::Content::Notification::Dislike`.

---
+++ src/models/relationship/content/notification/dislike.cr
@@ -0,0 +1,13 @@
+require "../../../relationship"
+require "../notification"
+
+class Relationship
+  class Content
+    class Notification < Relationship
+      class Dislike < Notification
+        belongs_to activity, class_name: ActivityPub::Activity::Dislike, foreign_key: to_iri, primary_key: iri
+        validates(activity) { "missing: #{to_iri}" unless activity? }
+      end
+    end
+  end
+end

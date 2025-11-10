0b802543da58505d6bc54eec554af1070a60e737
Todd Sundsted <todd@sundsted.com>
2025-11-09 17:25:08 -0800

    Add `config.options` support to the JS chart controller.

--- src/assets/js/controllers/chart_controller.js
+++ src/assets/js/controllers/chart_controller.js
@@ -2,17 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static get targets() {
-    return [ "labels", "datasets" ]
+    return [ "labels", "datasets", "options" ]
   }
 
   async connect() {
     let Chart = (await import("chart.js/auto")).default
-    this.chart = new Chart(this.element.getContext("2d"), {
+    const config = {
       data: {
         labels: JSON.parse(this.labelsTarget.textContent),
         datasets: JSON.parse(this.datasetsTarget.textContent)
       }
-    })
+    }
+
+    if (this.hasOptionsTarget) {
+      config.options = JSON.parse(this.optionsTarget.textContent)
+    }
+
+    this.chart = new Chart(this.element.getContext("2d"), config)
   }
 
   disconnect() {

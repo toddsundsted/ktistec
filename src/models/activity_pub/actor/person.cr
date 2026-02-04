a8fa3bbc43070eca4e9375e8013f52647af5b531
Todd Sundsted <todd@sundsted.com>
2026-02-04 13:26:49 -0500

    Permit some type changes.
    This allows Ktistec to gracefully deal with types (remote actors) that
    change externally.

--- src/models/activity_pub/actor/person.cr
+++ src/models/activity_pub/actor/person.cr
@@ -2,5 +2,11 @@ require "../actor"
 
 class ActivityPub::Actor
   class Person < ActivityPub::Actor
+    ALLOWED_TYPE_MIGRATIONS = [
+      "ActivityPub::Actor::Application",
+      "ActivityPub::Actor::Group",
+      "ActivityPub::Actor::Organization",
+      "ActivityPub::Actor::Service",
+    ]
   end
 end

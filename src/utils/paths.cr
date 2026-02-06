0220979e949ec6b6e90cbe5cc7275963509be2ef
Todd Sundsted <todd@sundsted.com>
2026-02-05 19:19:34 -0500

    Add "/remote/objects/:id/fetch/quote" endpoint.

--- src/utils/paths.cr
+++ src/utils/paths.cr
@@ -152,6 +152,10 @@ module Utils::Paths
     "#{Utils::Paths.remote_object_path({{object}})}/reply"
   end
 
+  macro fetch_quote_path(object = nil)
+    "#{Utils::Paths.remote_object_path({{object}})}/fetch/quote"
+  end
+
   macro bookmark_path(object = nil)
     "#{Utils::Paths.remote_object_path({{object}})}/bookmark"
   end

f7406039f2d5dae70111c463f1c0814ce5952c57
Todd Sundsted <todd@sundsted.com>
2026-02-08 21:12:50 -0500

    Document.

--- src/models/activity_pub/activity/quote_request.cr
+++ src/models/activity_pub/activity/quote_request.cr
@@ -8,6 +8,10 @@ class ActivityPub::Activity
       false
     end
 
+    # NOTE: per FEP-044F, "The `QuoteRequest` activity uses the
+    # `object` property to refer to the quoted object, and the
+    # `instrument` property to refer to the quote post."
+
     belongs_to object, class_name: ActivityPub::Object, foreign_key: object_iri, primary_key: iri
     belongs_to instrument, class_name: ActivityPub::Object, foreign_key: instrument_iri, primary_key: iri
   end

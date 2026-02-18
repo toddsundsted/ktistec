f03917039ea1aecb5193135f196ce63b15ecc88c
Todd Sundsted <todd@sundsted.com>
2026-02-18 16:00:20 -0500

    Dry up quote template specs.

--- spec/views/quote_spec.cr
+++ spec/views/quote_spec.cr
@@ -28,11 +28,15 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
 
   let(env) { make_env("GET", "/objects") }
 
+  MESSAGE_TEXT_PATH = "//*[contains(@class,'quoted-object')][not(section)]//em/text()"
+  BUTTON_TEXT_XPATH = "//*[contains(@class,'quoted-object')][not(section)]//button/text()"
+  CONTENT_XPATH     = "//section[@class='ui feed']//div[@class='content']"
+
   context "when not cached" do
     before_each { object.assign(quote_iri: "https://remote/objects/123").save }
 
     it "renders reload button" do
-      expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).not_to be_empty
+      expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to contain_exactly("Load quoted post")
     end
 
     it "renders turbo-frame target" do
@@ -40,7 +44,7 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
     end
 
     it "does not render quoted post" do
-      expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+      expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
     end
   end
 
@@ -52,7 +56,7 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
     before_each { actor.assign(icon: "https://test.test/image.jpg").save }
 
     it "renders quoted post" do
-      expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).not_to be_empty
+      expect(subject.xpath_nodes(CONTENT_XPATH)).not_to be_empty
     end
 
     it "renders quoted author's icon" do
@@ -71,8 +75,8 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
       expect(subject.xpath_nodes("//div[@class='extra text']")).to have("This is a quoted post.")
     end
 
-    it "does not render reload button" do
-      expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
+    it "does not render buttons" do
+      expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
     end
 
     context "with attachments" do
@@ -91,7 +95,7 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
       before_each { quote.assign(quote: other).save }
 
       it "does not render nested quote card" do
-        expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']").size).to eq(1)
+        expect(subject.xpath_nodes(CONTENT_XPATH).size).to eq(1)
       end
     end
 
@@ -99,15 +103,15 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
       before_each { quote.delete! }
 
       it "renders 'This post is deleted!' message" do
-        expect(subject.xpath_nodes("//em[text()='This post is deleted!']")).not_to be_empty
+        expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("This post is deleted!")
       end
 
       it "does not render quoted content" do
-        expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+        expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
       end
 
-      it "does not render a reload button" do
-        expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
+      it "does not render buttons" do
+        expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
       end
     end
 
@@ -115,15 +119,15 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
       before_each { quote.block! }
 
       it "renders 'This post is blocked!' message" do
-        expect(subject.xpath_nodes("//em[text()='This post is blocked!']")).not_to be_empty
+        expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("This post is blocked!")
       end
 
       it "does not render quoted content" do
-        expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+        expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
       end
 
-      it "does not render a reload button" do
-        expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
+      it "does not render buttons" do
+        expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
       end
     end
 
@@ -133,38 +137,30 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
       before_each { quote.assign(attributed_to: other).save }
 
       it "renders 'This quote cannot be verified.' message" do
-        expect(subject.xpath_nodes("//em[text()='This quote cannot be verified.']")).not_to be_empty
+        expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("This quote cannot be verified.")
       end
 
       it "does not render the quoted content" do
-        expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+        expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
       end
 
-      it "does not render a reload button" do
-        expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
-      end
-
-      it "does not render a verify button" do
-        expect(subject.xpath_nodes("//button[contains(text(),'Verify quote')]")).to be_empty
+      it "does not render buttons" do
+        expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
       end
 
       context "and quote_authorization_iri exists but authorization not cached" do
         before_each { object.assign(quote_authorization_iri: "https://remote/authorizations/123").save }
 
         it "renders 'This quote has not been verified.' message" do
-          expect(subject.xpath_nodes("//em[text()='This quote has not been verified.']")).not_to be_empty
+          expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("This quote has not been verified.")
         end
 
         it "does not render the quoted content" do
-          expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
-        end
-
-        it "does not render a reload button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
+          expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
         end
 
         it "renders a verify button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Verify quote')]")).not_to be_empty
+          expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to contain_exactly("Verify quote")
         end
       end
 
@@ -173,16 +169,16 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
 
         before_each { object.assign(quote_authorization_iri: quote_authorization.iri).save }
 
+        it "does not render message" do
+          expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to be_empty
+        end
+
         it "renders quoted post" do
-          expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).not_to be_empty
+          expect(subject.xpath_nodes(CONTENT_XPATH)).not_to be_empty
         end
 
-        it "does not render a reload button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
-        end
-
-        it "does not render a verify button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Verify quote')]")).to be_empty
+        it "does not render buttons" do
+          expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
         end
       end
 
@@ -190,19 +186,15 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
         let(error_message) { "Authorization does not match this quote." }
 
         it "renders the error message" do
-          expect(subject.xpath_nodes("//em[text()='Authorization does not match this quote.']")).not_to be_empty
+          expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("Authorization does not match this quote.")
         end
 
         it "does not render the quoted content" do
-          expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+          expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
         end
 
-        it "does not render a reload button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
-        end
-
-        it "does not render a verify button" do
-          expect(subject.xpath_nodes("//button[contains(text(),'Verify quote')]")).to be_empty
+        it "does not render buttons" do
+          expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
         end
       end
     end
@@ -212,15 +204,15 @@ Spectator.describe "views/partials/object/content/quote.html.slang" do
     let(failed) { true }
 
     it "renders 'Failed to load!' message" do
-      expect(subject.xpath_nodes("//em[text()='Failed to load!']")).not_to be_empty
+      expect(subject.xpath_nodes(MESSAGE_TEXT_PATH)).to contain_exactly("Failed to load!")
     end
 
     it "does not render quoted content" do
-      expect(subject.xpath_nodes("//section[@class='ui feed']//div[@class='content']")).to be_empty
+      expect(subject.xpath_nodes(CONTENT_XPATH)).to be_empty
     end
 
-    it "does not render a reload button" do
-      expect(subject.xpath_nodes("//button[contains(text(),'Load quoted post')]")).to be_empty
+    it "does not render buttons" do
+      expect(subject.xpath_nodes(BUTTON_TEXT_XPATH)).to be_empty
     end
   end
 end

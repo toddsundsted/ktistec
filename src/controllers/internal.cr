b80e8558be55a4d5abb7a16f72db14c57791ace0
Todd Sundsted <todd@sundsted.com>
2026-06-08 09:42:11 -0400

    Expose authenticated username in probe response.

--- src/controllers/internal.cr
+++ src/controllers/internal.cr
@@ -8,7 +8,11 @@ class InternalController
   skip_auth ["/_internal/authenticated"], GET
 
   get "/_internal/authenticated" do |env|
-    unauthorized unless env.account?
-    halt env, status_code: 204, response: ""
+    if (account = env.account?)
+      env.response.headers["X-Username"] = account.username
+      halt env, status_code: 204, response: ""
+    else
+      unauthorized
+    end
   end
 end
